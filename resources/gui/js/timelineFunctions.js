d4c4c13c92d9c198b6b7c1421d4648558937ffd3
Müller <muel_jd@sc-010144l.intra.dlr.de>
2019-08-20 09:39:20 +0200

    :beetle: Fix Uncaught TypeError

--- resources/gui/js/timelineFunctions.js
+++ resources/gui/js/timelineFunctions.js
@@ -145,12 +145,12 @@ noUiSlider.create(range, {
 
 
 var items = new vis.DataSet([
-    {id: 1, content: 'item 1', start: '2014-04-20'},
-    {id: 2, content: 'item 2', start: '2014-04-14'},
-    {id: 3, content: 'item 3', start: '2014-04-18'},
-    {id: 4, content: 'item 4', start: '2014-04-16', end: '2014-04-19'},
-    {id: 5, content: 'item 5', start: '2014-04-25'},
-    {id: 6, content: 'item 6', start: '2014-04-27', type: 'point'}
+    {id: 1, content: 'item 1', start: new Date('1960-04-20')},
+    {id: 2, content: 'item 2', start: new Date('1977-04-14')},
+    {id: 3, content: 'item 3', start: new Date('1983-04-18')},
+    {id: 4, content: 'item 4', start: new Date('1990-04-16'), end: new Date('1990-04-19')},
+    {id: 5, content: 'item 5', start: new Date('2000-04-25')},
+    {id: 6, content: 'item 6', start: new Date('2018-04-27')}
   ]);
 // Create a Timeline
 timeline = new vis.Timeline(container, items, options);
