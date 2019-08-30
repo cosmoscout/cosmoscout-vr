d3f7810b576178235d960e894755b2d0766cdc3f
Müller <muel_jd@sc-010144l.intra.dlr.de>
2019-08-30 13:49:36 +0200

    :wrench: Improve styel of the time control

--- resources/gui/js/fade.js
+++ resources/gui/js/fade.js
@@ -13,19 +13,48 @@ function toggleLock() {
 }
 
 
-function mouseEnterCallback (evt){
+function mouseEnterTimeline (evt){
     if(!locked) {
         document.getElementById('divContainer').classList.add('visible');
     }
 }
 
-function mouseLeaveCallback (evt) {
+function mouseLeaveTimenavigation (evt) {
     if(!locked) {
         document.getElementById('divContainer').classList.remove('visible');
     }
 }
 
+function mouseEnterTimeControl(evt) {
+    document.getElementById("increaseControl").classList.add('mouseNear');
+    document.getElementById("decreaseControl").classList.add('mouseNear');
+}
+
+function mouseLeaveTimeControl(evt) {
+    document.getElementById("increaseControl").classList.remove('mouseNear');
+    document.getElementById("decreaseControl").classList.remove('mouseNear');
+}
+
+function enterTimeButtons(evt) {
+    document.getElementById("increaseControl").classList.add('mouseOver');
+    document.getElementById("decreaseControl").classList.add('mouseOver');
+}
+
+function leaveTimeButtons(evt) {
+    document.getElementById("increaseControl").classList.remove('mouseOver');
+    document.getElementById("decreaseControl").classList.remove('mouseOver');
+}
+
 document.getElementById("btnLock").onclick = toggleLock;
 
-document.getElementById("visualization").onmouseenter = mouseEnterCallback;
-document.getElementById("divContainer").onmouseleave = mouseLeaveCallback;
+document.getElementById("visualization").onmouseenter = mouseEnterTimeline;
+document.getElementById("divContainer").onmouseleave = mouseLeaveTimenavigation;
+
+document.getElementById("timeControl").onmouseenter = mouseEnterTimeControl;
+document.getElementById("timeControl").onmouseleave = mouseLeaveTimeControl;
+
+document.getElementById("increaseControl").onmouseenter = enterTimeButtons;
+document.getElementById("increaseControl").onmouseleave = leaveTimeButtons;
+
+document.getElementById("decreaseControl").onmouseenter = enterTimeButtons;
+document.getElementById("decreaseControl").onmouseleave = leaveTimeButtons;
