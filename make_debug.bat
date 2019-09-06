9c9f1e6f01a6fd9c365062a9e0c8afb1377dc752
Simon Schneegans <simon.schneegans@dlr.de>
2019-09-06 11:18:47 +0200

    :tada: Allow different CMake generators to be used in build scripts

--- make_debug.bat
+++ make_debug.bat
@@ -6,6 +6,21 @@ rem      and may be used under the terms of the MIT license. See the LICENSE fil
 rem                         Copyright: (c) 2019 German Aerospace Center (DLR)                      #
 rem ---------------------------------------------------------------------------------------------- #
 
+rem ---------------------------------------------------------------------------------------------- #
+rem Usage:                                                                                         #
+rem    make_debug.bat [additional CMake flags, defaults to -G "Visual Studio 15 Win64"]            #
+rem Examples:                                                                                      #
+rem    make_debug.bat                                                                              #
+rem    make_debug.bat -G "Visual Studio 15 Win64"                                                  #
+rem    make_debug.bat -G "Visual Studio 16 2019" -A x64                                            #
+rem ---------------------------------------------------------------------------------------------- #
+
+rem The CMake generator and other flags can be passed as parameters.
+set CMAKE_FLAGS=-G "Visual Studio 15 Win64"
+IF NOT "%*"=="" (
+  SET CMAKE_FLAGS=%*
+)
+
 rem create some required variables -----------------------------------------------------------------
 
 rem This directory should contain the top-level CMakeLists.txt - it is assumed to reside in the same
@@ -33,7 +48,7 @@ if exist "%BUILD_DIR%" goto BUILD_DIR_CREATED
 rem configure, compile & install -------------------------------------------------------------------
 
 cd "%BUILD_DIR%"
-cmake -G "Visual Studio 15 Win64" -DCMAKE_INSTALL_PREFIX="%INSTALL_DIR%"^
+cmake %CMAKE_FLAGS% -DCMAKE_INSTALL_PREFIX="%INSTALL_DIR%"^
       -DCOSMOSCOUT_EXTERNALS_DIR="%EXTERNALS_INSTALL_DIR%"^
       -DCMAKE_EXPORT_COMPILE_COMMANDS=On "%CMAKE_DIR%"
 
--- make_externals.sh
+++ make_externals.sh
@@ -6,12 +6,25 @@
 #                         Copyright: (c) 2019 German Aerospace Center (DLR)                        #
 # ------------------------------------------------------------------------------------------------ #
 
-# ------------------------------------------------------------------------------------------------ #
-# Make sure to run "git submodule update --init" before executing this script!                     #
-# ------------------------------------------------------------------------------------------------ #
-
 # exit on error
 set -e
+
+# ------------------------------------------------------------------------------------------------ #
+# Make sure to run "git submodule update --init" before executing this script!                     #
+# Usage:                                                                                           #
+#    ./make_externals.sh [additional CMake flags, defaults to -G "Eclipse CDT4 - Unix Makefiles"]  #
+# Examples:                                                                                        #
+#    ./make_externals.sh                                                                           #
+#    ./make_externals.sh -G "Unix Makefiles"                                                       #
+#    ./make_externals.sh -G "Eclipse CDT4 - Unix Makefiles" -DCMAKE_CXX_COMPILER_LAUNCHER=ccache \ #
+#                        -DCMAKE_C_COMPILER_LAUNCHER=ccache                                        #
+# ------------------------------------------------------------------------------------------------ #
+
+# The CMake generator and other flags can be passed as parameters.
+CMAKE_FLAGS=(-G "Eclipse CDT4 - Unix Makefiles")
+if [ $# -ne 0 ]; then
+  CMAKE_FLAGS=( "$@" )
+fi
 
 # Create some required variables. ------------------------------------------------------------------
 
@@ -34,15 +47,6 @@ cmake -E make_directory "$INSTALL_DIR/share"
 cmake -E make_directory "$INSTALL_DIR/bin"
 cmake -E make_directory "$INSTALL_DIR/include"
 
-# The optional parameter --with-ccache enables the ccache support of CMake.
-# ccache must be installed on your system.
-if [[ $* == *--with-ccache* ]]
-then
-    CCACHE_FLAGS="-DCMAKE_CXX_COMPILER_LAUNCHER=ccache -DCMAKE_C_COMPILER_LAUNCHER=ccache"
-else
-    CCACHE_FLAGS=""
-fi
-
 # glew ---------------------------------------------------------------------------------------------
 
 echo ""
@@ -56,7 +60,7 @@ cd "$BUILD_DIR/glew/extracted"
 cmake -E tar xzf ../glew-2.1.0.tgz
 cd ..
 
-cmake -G "Eclipse CDT4 - Unix Makefiles" $CCACHE_FLAGS -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
+cmake "${CMAKE_FLAGS[@]}" -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
       -DCMAKE_INSTALL_LIBDIR=lib \
       -DCMAKE_BUILD_TYPE=Release "$BUILD_DIR/glew/extracted/glew-2.1.0/build/cmake"
 cmake --build . --target install --parallel 8
@@ -68,7 +72,7 @@ echo "Building and installing freeglut ..."
 echo ""
 
 cmake -E make_directory "$BUILD_DIR/freeglut" && cd "$BUILD_DIR/freeglut"
-cmake -G "Eclipse CDT4 - Unix Makefiles" $CCACHE_FLAGS -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
+cmake "${CMAKE_FLAGS[@]}" -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
       -DCMAKE_INSTALL_LIBDIR=lib \
       -DCMAKE_BUILD_TYPE=Release "$EXTERNALS_DIR/freeglut/freeglut/freeglut"
 cmake --build . --target install --parallel 8
@@ -83,7 +87,7 @@ echo "Building and installing c-ares ..."
 echo ""
 
 cmake -E make_directory "$BUILD_DIR/c-ares" && cd "$BUILD_DIR/c-ares"
-cmake -G "Eclipse CDT4 - Unix Makefiles" $CCACHE_FLAGS -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
+cmake "${CMAKE_FLAGS[@]}" -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
       -DCMAKE_BUILD_TYPE=Release "$EXTERNALS_DIR/c-ares"
 cmake --build . --target install --parallel 8
 
@@ -94,7 +98,7 @@ echo "Building and installing curl ..."
 echo ""
 
 cmake -E make_directory "$BUILD_DIR/curl" && cd "$BUILD_DIR/curl"
-cmake -G "Eclipse CDT4 - Unix Makefiles" $CCACHE_FLAGS -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
+cmake "${CMAKE_FLAGS[@]}" -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
       -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTING=OFF -DBUILD_CURL_EXE=OFF -DENABLE_ARES=ON \
       -DCARES_INCLUDE_DIR="$INSTALL_DIR/include" \
       -DCARES_LIBRARY="$INSTALL_DIR/lib/libcares.so" \
@@ -109,7 +113,7 @@ echo "Building and installing curlpp ..."
 echo ""
 
 cmake -E make_directory "$BUILD_DIR/curlpp" && cd "$BUILD_DIR/curlpp"
-cmake -G "Eclipse CDT4 - Unix Makefiles" $CCACHE_FLAGS -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
+cmake "${CMAKE_FLAGS[@]}" -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
       -DCURL_INCLUDE_DIR="$INSTALL_DIR/include" \
       -DCURL_LIBRARY="$INSTALL_DIR/lib/libcurl.so" \
       -DCMAKE_INSTALL_LIBDIR=lib \
@@ -123,7 +127,7 @@ echo "Building and installing libtiff ..."
 echo ""
 
 cmake -E make_directory "$BUILD_DIR/libtiff" && cd "$BUILD_DIR/libtiff"
-cmake -G "Eclipse CDT4 - Unix Makefiles" $CCACHE_FLAGS -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
+cmake "${CMAKE_FLAGS[@]}" -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
       -DCMAKE_INSTALL_FULL_LIBDIR=lib \
       -DCMAKE_BUILD_TYPE=Release "$EXTERNALS_DIR/libtiff"
 cmake --build . --target install --parallel 8
@@ -162,7 +166,7 @@ echo "Building and installing opensg-1.8 ..."
 echo ""
 
 cmake -E make_directory "$BUILD_DIR/opensg-1.8" && cd "$BUILD_DIR/opensg-1.8"
-cmake -G "Eclipse CDT4 - Unix Makefiles" $CCACHE_FLAGS -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
+cmake "${CMAKE_FLAGS[@]}" -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
       -DGLUT_INCLUDE_DIR="$INSTALL_DIR/include" -DGLUT_LIBRARY="$INSTALL_DIR/lib/libglut.so" \
       -DOPENSG_BUILD_TESTS=Off -DCMAKE_BUILD_TYPE=Release "$EXTERNALS_DIR/opensg-1.8"
 cmake --build . --target install --parallel 8
@@ -174,7 +178,7 @@ echo "Building and installing vista ..."
 echo ""
 
 cmake -E make_directory "$BUILD_DIR/vista" && cd "$BUILD_DIR/vista"
-cmake -G "Eclipse CDT4 - Unix Makefiles" $CCACHE_FLAGS -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
+cmake "${CMAKE_FLAGS[@]}" -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
       -DCMAKE_CXX_FLAGS="-std=c++11" -DVISTADRIVERS_BUILD_3DCSPACENAVIGATOR=On \
       -DVISTADEMO_ENABLED=Off -DCMAKE_BUILD_TYPE=Release -DOPENSG_ROOT_DIR="$INSTALL_DIR" \
       "$EXTERNALS_DIR/vista"
@@ -211,7 +215,7 @@ cmake -E tar xfj ../$CEF_VERSION.tar.bz2
 rm -rf $CEF_VERSION/tests # we dont want the example applications
 cd ..
 
-cmake -G "Eclipse CDT4 - Unix Makefiles" $CCACHE_FLAGS -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
+cmake "${CMAKE_FLAGS[@]}" -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
       -DCEF_COMPILER_FLAGS="-Wno-undefined-var-template" \
       -DCMAKE_BUILD_TYPE=Release "$BUILD_DIR/cef/extracted/$CEF_VERSION"
 cmake --build . --parallel 8
