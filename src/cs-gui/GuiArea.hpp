427eee65690c34e89862edee4a5821aba1ea80ba
Simon Schneegans <simon.schneegans@dlr.de>
2019-09-17 13:45:34 +0200

    :sparkles: Apply clang-format

--- src/cs-gui/GuiArea.hpp
+++ src/cs-gui/GuiArea.hpp
@@ -28,8 +28,7 @@ namespace cs::gui {
 class GuiItem;
 
 /// A container which holds a collection of GuiItems.
-class CS_GUI_EXPORT GuiArea
-{
+class CS_GUI_EXPORT GuiArea {
 
  public:
   GuiArea()          = default;
