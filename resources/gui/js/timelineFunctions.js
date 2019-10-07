c5a01f52d9be2e6a7a6c30e97f65bf5b5cfcfdbe
Müller <muel_jd@sc-010144l.intra.dlr.de>
2019-10-07 15:22:59 +0200

    :wrench: Change editing options

--- resources/gui/js/timelineFunctions.js
+++ resources/gui/js/timelineFunctions.js
@@ -149,7 +149,7 @@ var editingDoneOpt = {
         add: true,         // add new items by double tapping
         updateTime: true,  // drag items horizontally
         updateGroup: false, // drag items from one group to another
-        remove: true,       // delete an item by tapping the delete button top right
+        remove: false,       // delete an item by tapping the delete button top right
         overrideItems: false  // allow these options to override item.editable
     }
 }
