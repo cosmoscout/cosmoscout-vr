33c507c894850d398a5f5551f38c7791a7f722e2
Simon Schneegans <simon.schneegans@dlr.de>
2019-10-25 13:45:30 +0200

    :tada: Initial attempt on HDR stars

--- resources/gui/js/sidebar.js
+++ resources/gui/js/sidebar.js
@@ -183,7 +183,7 @@ $(document).ready(function () {
     init_slider("set_exposure_compensation", -10, 10, 0.5, [0]);
     init_slider("set_exposure_adaption_speed", 0, 20, 0.1, [3]);
     init_slider("set_ambient_light", 0.0, 1.0, 0.001, [0.25]);
-    init_slider("set_exposure_range", -30.0, 30, 0.1, [-15, 1.5]);
+    init_slider("set_exposure_range", -30.0, 30, 0.1, [-15, 10]);
     init_slider("set_glow_intensity", 0.0, 1, 0.01, [0.1]);
 
     $(document).on('click', '.item-create-button', function () {
