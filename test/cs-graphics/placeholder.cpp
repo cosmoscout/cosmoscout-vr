00fc33d3aa3dc35784233873dd6c94e34c737d14
Jonas Gilg <jonas.gilg@dlr.de>
2019-12-13 16:51:33 +0100

    :tada: Tests now reside in a test folder with the same structure as the src folder.

---
+++ test/cs-graphics/placeholder.cpp
@@ -0,0 +1,11 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//                               This file is part of CosmoScout VR                               //
+//      and may be used under the terms of the MIT license. See the LICENSE file for details.     //
+//                        Copyright: (c) 2019 German Aerospace Center (DLR)                       //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "../../src/cs-utils/doctest.hpp"
+
+TEST_CASE("GRAPHICS PLACEHOLDER") {
+  CHECK(true);
+}
---
+++ test/cs-utils/utils.cpp
@@ -0,0 +1,21 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//                               This file is part of CosmoScout VR                               //
+//      and may be used under the terms of the MIT license. See the LICENSE file for details.     //
+//                        Copyright: (c) 2019 German Aerospace Center (DLR)                       //
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "../../src/cs-utils/doctest.hpp"
+#include "../../src/cs-utils/utils.hpp"
+
+namespace cs::utils {
+TEST_CASE("string contains") {
+  CHECK(contains("", ""));
+  CHECK(contains("ab", "a"));
+  CHECK(contains("aa", "a"));
+
+  CHECK(!contains("", "a"));
+  CHECK(!contains("b", "a"));
+  CHECK(!contains("ab", "ba"));
+  CHECK(!contains("a", " "));
+};
+}
