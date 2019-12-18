15b0f9635306a7ea252639b3392b5e8d017defd7
Hannes Kruse <Hannes.Kruse@dlr.de>
2019-12-18 15:29:43 +0100

    :sparkles: Add eslint
    Node needed.
    Run `npm install`
    Lint and fix javascript files: `npm run lint-fix`

---
+++ resources/gui/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    "no-underscore-dangle": ["error", { "allowAfterThis": true }]
+  },
+  "parser": "babel-eslint"
+};
