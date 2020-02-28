4bf647dc7fc181b6ede6be166668e187c7ea8326
flat_ma <markus.flatken@dlr.de>
2020-02-28 11:13:15 +0100

    :arrow_up_small: Add SQLite3

--- make_externals.bat
+++ make_externals.bat
@@ -80,24 +80,17 @@ cmake -E make_directory "%INSTALL_DIR%/share"
 cmake -E make_directory "%INSTALL_DIR%/bin"
 cmake -E make_directory "%INSTALL_DIR%/include"
 
-rem gdal 3.0 --------------------------------------------------------------------------------------------
+rem # SQLite3 -----------------------------------------------------------------------------------------
 
-echo.
-echo Downloading and installing gdal ...
-echo.
+echo .
+echo Building and installing sqlite3 ...
+echo .
 
-cmake -E make_directory "%BUILD_DIR%/gdal/extracted" && cd "%BUILD_DIR%/gdal"
-powershell.exe -command Invoke-WebRequest -Uri http://download.gisinternals.com/sdk/downloads/release-1911-x64-gdal-3-0-2-mapserver-7-4-2-libs.zip -OutFile gdal.zip
-powershell.exe -command Invoke-WebRequest -Uri http://download.gisinternals.com/sdk/downloads/release-1911-x64-gdal-3-0-2-mapserver-7-4-2.zip -OutFile gdal_bin.zip
+cmake -E make_directory "%BUILD_DIR%/sqlite3" && cd "%BUILD_DIR%/sqlite3"
+cmake %CMAKE_FLAGS% -DCMAKE_INSTALL_PREFIX="%INSTALL_DIR%"^
+      -DBUILD_TESTING=off "%EXTERNALS_DIR%/sqlite3"
+cmake --build . --config %BUILD_TYPE% --target install --parallel 8
 
-
-cd "%BUILD_DIR%/gdal/extracted"
-cmake -E tar xfvj ../gdal.zip
-cmake -E tar xfvj ../gdal_bin.zip
-
-cmake -E copy_directory "%BUILD_DIR%/gdal/extracted/include"                   "%INSTALL_DIR%/gdal/include/"
-cmake -E copy_directory "%BUILD_DIR%/gdal/extracted/lib"        			   "%INSTALL_DIR%/gdal/lib"
-cmake -E copy_directory "%BUILD_DIR%/gdal/extracted/bin"        			   "%INSTALL_DIR%/gdal/lib"
 rem # VTK -----------------------------------------------------------------------------------------
 
 echo .
@@ -117,8 +110,28 @@ echo .
 
 cmake -E make_directory "%BUILD_DIR%/ttk" && cd "%BUILD_DIR%/ttk"
 cmake %CMAKE_FLAGS% -DCMAKE_INSTALL_PREFIX="%INSTALL_DIR%"^
-      -DTTK_BUILD_PARAVIEW_PLUGINS=Off -DTTK_ENABLE_GRAPHVIZ=Off -DBUILD_TESTING=off "%EXTERNALS_DIR%/ttk"
+      -DTTK_BUILD_PARAVIEW_PLUGINS=Off -DTTK_ENABLE_GRAPHVIZ=Off^
+	  -DSQLITE3_INCLUDE_DIR="%INSTALL_DIR%/include/sqlite3" -DBUILD_TESTING=off "%EXTERNALS_DIR%/ttk"
 cmake --build . --config %BUILD_TYPE% --target install --parallel 8
+
+rem gdal 3.0 --------------------------------------------------------------------------------------------
+
+echo.
+echo Downloading and installing gdal ...
+echo.
+
+cmake -E make_directory "%BUILD_DIR%/gdal/extracted" && cd "%BUILD_DIR%/gdal"
+powershell.exe -command Invoke-WebRequest -Uri http://download.gisinternals.com/sdk/downloads/release-1911-x64-gdal-3-0-2-mapserver-7-4-2-libs.zip -OutFile gdal.zip
+powershell.exe -command Invoke-WebRequest -Uri http://download.gisinternals.com/sdk/downloads/release-1911-x64-gdal-3-0-2-mapserver-7-4-2.zip -OutFile gdal_bin.zip
+
+
+cd "%BUILD_DIR%/gdal/extracted"
+cmake -E tar xfvj ../gdal.zip
+cmake -E tar xfvj ../gdal_bin.zip
+
+cmake -E copy_directory "%BUILD_DIR%/gdal/extracted/include"                   "%INSTALL_DIR%/gdal/include/"
+cmake -E copy_directory "%BUILD_DIR%/gdal/extracted/lib"        			   "%INSTALL_DIR%/gdal/lib"
+cmake -E copy_directory "%BUILD_DIR%/gdal/extracted/bin"        			   "%INSTALL_DIR%/gdal/lib"
 
 rem glew -------------------------------------------------------------------------------------------
 :glew
