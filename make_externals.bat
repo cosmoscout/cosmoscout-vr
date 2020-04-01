338367d101913c5c9df506ad903c0357e1b55000
Flatken <markus.flatken@dlr.de>
2020-04-01 12:53:29 +0200

    :arrow_up_small: Update GDAL dependency

--- make_externals.bat
+++ make_externals.bat
@@ -80,6 +80,24 @@ cmake -E make_directory "%INSTALL_DIR%/share"
 cmake -E make_directory "%INSTALL_DIR%/bin"
 cmake -E make_directory "%INSTALL_DIR%/include"
 
+rem gdal 3.0.4 --------------------------------------------------------------------------------------------
+
+echo.
+echo Downloading and installing gdal ...
+echo.
+
+cmake -E make_directory "%BUILD_DIR%/gdal/extracted" && cd "%BUILD_DIR%/gdal"
+powershell.exe -command Invoke-WebRequest -Uri http://download.gisinternals.com/sdk/downloads/release-1911-x64-gdal-3-0-4-mapserver-7-4-3-libs.zip -OutFile gdal.zip
+powershell.exe -command Invoke-WebRequest -Uri http://download.gisinternals.com/sdk/downloads/release-1911-x64-gdal-3-0-4-mapserver-7-4-3.zip -OutFile gdal_bin.zip
+
+cd "%BUILD_DIR%/gdal/extracted"
+cmake -E tar xfvj ../gdal.zip
+cmake -E tar xfvj ../gdal_bin.zip
+
+cmake -E copy_directory "%BUILD_DIR%/gdal/extracted/include"                   "%INSTALL_DIR%/gdal/include/"
+cmake -E copy_directory "%BUILD_DIR%/gdal/extracted/lib"        			   "%INSTALL_DIR%/gdal/lib"
+cmake -E copy_directory "%BUILD_DIR%/gdal/extracted/bin"        			   "%INSTALL_DIR%/gdal/lib"
+
 rem # SQLite3 -----------------------------------------------------------------------------------------
 
 echo .
@@ -114,16 +132,15 @@ cmake %CMAKE_FLAGS% -DCMAKE_INSTALL_PREFIX="%INSTALL_DIR%"^
 	  -DSQLITE3_INCLUDE_DIR="%INSTALL_DIR%/include/sqlite3" -DBUILD_TESTING=off "%EXTERNALS_DIR%/ttk"
 cmake --build . --config %BUILD_TYPE% --target install --parallel 8
 
-rem gdal 3.0 --------------------------------------------------------------------------------------------
+rem gdal 3.0.4 --------------------------------------------------------------------------------------------
 
 echo.
 echo Downloading and installing gdal ...
 echo.
 
 cmake -E make_directory "%BUILD_DIR%/gdal/extracted" && cd "%BUILD_DIR%/gdal"
-powershell.exe -command Invoke-WebRequest -Uri http://download.gisinternals.com/sdk/downloads/release-1911-x64-gdal-3-0-2-mapserver-7-4-2-libs.zip -OutFile gdal.zip
-powershell.exe -command Invoke-WebRequest -Uri http://download.gisinternals.com/sdk/downloads/release-1911-x64-gdal-3-0-2-mapserver-7-4-2.zip -OutFile gdal_bin.zip
-
+powershell.exe -command Invoke-WebRequest -Uri http://download.gisinternals.com/sdk/downloads/release-1911-x64-gdal-3-0-4-mapserver-7-4-3-libs.zip -OutFile gdal.zip
+powershell.exe -command Invoke-WebRequest -Uri http://download.gisinternals.com/sdk/downloads/release-1911-x64-gdal-3-0-4-mapserver-7-4-3.zip -OutFile gdal_bin.zip
 
 cd "%BUILD_DIR%/gdal/extracted"
 cmake -E tar xfvj ../gdal.zip
