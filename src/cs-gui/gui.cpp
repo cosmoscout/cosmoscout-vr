5f72e2850bc5f0fbb5242514165572333528ad99
Simon Schneegans <code@simonschneegans.de>
2020-04-16 17:57:58 +0200

    :sparkles: Apply clang-format

--- src/cs-gui/gui.cpp
+++ src/cs-gui/gui.cpp
@@ -19,7 +19,8 @@ CefRefPtr<detail::WebApp> app;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void executeWebProcess(int argc, char* argv[]) { // NOLINT(modernize-avoid-c-arrays)
+// NOLINTNEXTLINE(modernize-avoid-c-arrays)
+void executeWebProcess(int argc, char* argv[]) {
   // NOLINTNEXTLINE(cppcoreguidelines-owning-memory)
   app        = new detail::WebApp(argc, argv, false);
   int result = CefExecuteProcess(app->GetArgs(), app, nullptr);
