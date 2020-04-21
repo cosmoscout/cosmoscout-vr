85e935483a7285873c1aaace36aacc0415a7e7ae
Simon Schneegans <code@simonschneegans.de>
2020-04-21 12:07:23 +0200

    :wrench: Disable SPDLOG_ENABLE_PCH

--- make_externals.sh
+++ make_externals.sh
@@ -158,8 +158,7 @@ echo ""
 
 cmake -E make_directory "$BUILD_DIR/spdlog" && cd "$BUILD_DIR/spdlog"
 cmake "${CMAKE_FLAGS[@]}" -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
-      -DCMAKE_POSITION_INDEPENDENT_CODE=On -DSPDLOG_ENABLE_PCH=On \
-      -DCMAKE_BUILD_TYPE=$BUILD_TYPE "$EXTERNALS_DIR/spdlog"
+      -DCMAKE_POSITION_INDEPENDENT_CODE=On -DCMAKE_BUILD_TYPE=$BUILD_TYPE "$EXTERNALS_DIR/spdlog"
 cmake --build . --target install --parallel "$(nproc)"
 
 # jsonhpp ------------------------------------------------------------------------------------------
