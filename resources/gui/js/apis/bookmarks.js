71fc59c92609a5a95ae779a9586c6da778e0c718
Simon Schneegans <code@simonschneegans.de>
2020-05-06 16:43:23 +0200

    :wrench: Add more validity checks

--- resources/gui/js/apis/bookmarks.js
+++ resources/gui/js/apis/bookmarks.js
@@ -55,38 +55,43 @@ class BookmarksApi extends IApi {
 
     // Connect buttons setting fields to current values --------------------------------------------
 
-    document.querySelector("#bookmark-editor-start-date + div > button").onmouseup = () => {
-      this._startDateDiv.value =
-          CosmoScout.state.simulationTime.toISOString().replace('T', ' ').slice(0, 19);
-    };
+    document.querySelector("#bookmark-editor-start-date + div > button")
+        .addEventListener('click', () => {
+          this._startDateDiv.value =
+              CosmoScout.state.simulationTime.toISOString().replace('T', ' ').slice(0, 19);
+        });
 
-    document.querySelector("#bookmark-editor-end-date + div > button").onmouseup = () => {
-      this._endDateDiv.value =
-          CosmoScout.state.simulationTime.toISOString().replace('T', ' ').slice(0, 19);
-    };
+    document.querySelector("#bookmark-editor-end-date + div > button")
+        .addEventListener('click', () => {
+          this._endDateDiv.value =
+              CosmoScout.state.simulationTime.toISOString().replace('T', ' ').slice(0, 19);
+        });
 
-    document.querySelector("#bookmark-editor-frame + div > button").onmouseup = () => {
-      this._centerDiv.value = CosmoScout.state.activePlanetCenter;
-      this._frameDiv.value  = CosmoScout.state.activePlanetFrame;
-    };
+    document.querySelector("#bookmark-editor-frame + div > button")
+        .addEventListener('click', () => {
+          this._centerDiv.value = CosmoScout.state.activePlanetCenter;
+          this._frameDiv.value  = CosmoScout.state.activePlanetFrame;
+        });
 
-    document.querySelector("#bookmark-editor-location-z + div > button").onmouseup = () => {
-      this._locationXDiv.value = CosmoScout.state.observerPosition[0];
-      this._locationYDiv.value = CosmoScout.state.observerPosition[1];
-      this._locationZDiv.value = CosmoScout.state.observerPosition[2];
-    };
+    document.querySelector("#bookmark-editor-location-z + div > button")
+        .addEventListener('click', () => {
+          this._locationXDiv.value = CosmoScout.state.observerPosition[0];
+          this._locationYDiv.value = CosmoScout.state.observerPosition[1];
+          this._locationZDiv.value = CosmoScout.state.observerPosition[2];
+        });
 
-    document.querySelector("#bookmark-editor-rotation-w + div > button").onmouseup = () => {
-      this._rotationXDiv.value = CosmoScout.state.observerRotation[0];
-      this._rotationYDiv.value = CosmoScout.state.observerRotation[1];
-      this._rotationZDiv.value = CosmoScout.state.observerRotation[2];
-      this._rotationWDiv.value = CosmoScout.state.observerRotation[3];
-    };
+    document.querySelector("#bookmark-editor-rotation-w + div > button")
+        .addEventListener('click', () => {
+          this._rotationXDiv.value = CosmoScout.state.observerRotation[0];
+          this._rotationYDiv.value = CosmoScout.state.observerRotation[1];
+          this._rotationZDiv.value = CosmoScout.state.observerRotation[2];
+          this._rotationWDiv.value = CosmoScout.state.observerRotation[3];
+        });
 
     // Save bookmark on save button click ----------------------------------------------------------
 
-    this._saveButton.onmouseup = () => {
-      // First validate all fields.
+    this._saveButton.addEventListener('click', () => {
+      // First validate all fields. We check for completeness first, then for correct formatting.
       let nameGiven      = this._nameDiv.value != "";
       let startDateGiven = this._startDateDiv.value != "";
       let endDateGiven   = this._endDateDiv.value != "";
@@ -104,25 +109,47 @@ class BookmarksApi extends IApi {
       let locationRequired = anyRotationGiven || anyLocationGiven;
       let rotationRequired = anyRotationGiven;
 
-      let highlight = (div, doHighlight) => {
-        if (doHighlight) {
+      let allCorrect = true;
+
+      let markInvalid = (div, isInvalid, message) => {
+        if (isInvalid) {
           div.classList.add("is-invalid");
+          div.parentNode.querySelector(".invalid-feedback").textContent = message;
+          allCorrect                                                    = false;
         } else {
           div.classList.remove("is-invalid");
         }
       };
 
-      highlight(this._nameDiv, !nameGiven);
-      highlight(this._startDateDiv, endDateGiven && !startDateGiven);
-      highlight(this._centerDiv, !centerGiven && centerRequired);
-      highlight(this._frameDiv, !frameGiven && frameRequired);
-      highlight(this._locationXDiv, locationRequired && this._locationXDiv.value == "");
-      highlight(this._locationYDiv, locationRequired && this._locationYDiv.value == "");
-      highlight(this._locationZDiv, locationRequired && this._locationZDiv.value == "");
-      highlight(this._rotationXDiv, rotationRequired && this._rotationXDiv.value == "");
-      highlight(this._rotationYDiv, rotationRequired && this._rotationYDiv.value == "");
-      highlight(this._rotationZDiv, rotationRequired && this._rotationZDiv.value == "");
-      highlight(this._rotationWDiv, rotationRequired && this._rotationWDiv.value == "");
+      markInvalid(this._nameDiv, !nameGiven, "Please choose a name.");
+
+      markInvalid(this._startDateDiv, endDateGiven && !startDateGiven,
+          "You have to specify a start date if you set an end date.");
+
+      markInvalid(this._centerDiv, !centerGiven && centerRequired, "Both values are required.");
+
+      markInvalid(this._frameDiv, !frameGiven && frameRequired, "Both values are required.");
+
+      markInvalid(this._locationXDiv, locationRequired && this._locationXDiv.value == "",
+          "All values are required.");
+
+      markInvalid(this._locationYDiv, locationRequired && this._locationYDiv.value == "",
+          "All values are required.");
+
+      markInvalid(this._locationZDiv, locationRequired && this._locationZDiv.value == "",
+          "All values are required.");
+
+      markInvalid(this._rotationXDiv, rotationRequired && this._rotationXDiv.value == "",
+          "Provide all rotation values or none.");
+
+      markInvalid(this._rotationYDiv, rotationRequired && this._rotationYDiv.value == "",
+          "Provide all rotation values or none.");
+
+      markInvalid(this._rotationZDiv, rotationRequired && this._rotationZDiv.value == "",
+          "Provide all rotation values or none.");
+
+      markInvalid(this._rotationWDiv, rotationRequired && this._rotationWDiv.value == "",
+          "Provide all rotation values or none.");
 
       if (nameGiven && !(startDateGiven || endDateGiven || centerGiven || frameGiven ||
                            anyLocationGiven || anyRotationGiven)) {
@@ -130,7 +157,49 @@ class BookmarksApi extends IApi {
       } else {
         this._nothingGivenError.style.display = "none";
       }
-    };
+
+      // Abort if errors occurred.
+      if (!allCorrect) {
+        return;
+      }
+
+      // Now check some formatting of the position, rotation and date inputs.
+
+      // Based on https://stackoverflow.com/questions/3143070/javascript-regex-iso-datetime
+      let dateRegex =
+          /(^\d{4}-[01]\d-[0-3]\d [0-2]\d:[0-5]\d:[0-5]\d\.\d+$)|(^\d{4}-[01]\d-[0-3]\d [0-2]\d:[0-5]\d:[0-5]\d$)|(^\d{4}-[01]\d-[0-3]\d [0-2]\d:[0-5]\d$)/;
+      let dateError =
+          "Date must be in the format YYYY-MM-DD HH:MM:SS.fff with seconds and milliseconds being optional.";
+
+      if (startDateGiven) {
+        markInvalid(this._startDateDiv, !this._startDateDiv.value.match(dateRegex), dateError);
+      }
+
+      if (endDateGiven) {
+        markInvalid(this._endDateDiv, !this._endDateDiv.value.match(dateRegex), dateError);
+      }
+
+      let numRegex = /^[0-9]+(\.[0-9]*)?$/;
+      let numError = "Must be a number.";
+
+      if (anyLocationGiven) {
+        markInvalid(this._locationXDiv, !this._locationXDiv.value.match(numRegex), numError);
+        markInvalid(this._locationYDiv, !this._locationYDiv.value.match(numRegex), numError);
+        markInvalid(this._locationZDiv, !this._locationZDiv.value.match(numRegex), numError);
+      }
+
+      if (anyRotationGiven) {
+        markInvalid(this._rotationXDiv, !this._rotationXDiv.value.match(numRegex), numError);
+        markInvalid(this._rotationYDiv, !this._rotationYDiv.value.match(numRegex), numError);
+        markInvalid(this._rotationZDiv, !this._rotationZDiv.value.match(numRegex), numError);
+        markInvalid(this._rotationWDiv, !this._rotationWDiv.value.match(numRegex), numError);
+      }
+
+      // Abort if errors occurred.
+      if (!allCorrect) {
+        return;
+      }
+    });
   }
 
   /**
