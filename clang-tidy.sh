a70353a0f470db57d566ef22a787d175cef2bb4d
Andreas-Christoph Bernstein <andreas-christoph.bernstein@dlr.de>
2020-05-29 17:03:13 +0200

    :wrench: Capitalize build type in documentation and clang-tidy script

--- clang-tidy.sh
+++ clang-tidy.sh
@@ -7,17 +7,17 @@
 # ------------------------------------------------------------------------------------------------ #
 
 # For this script to work, CosmoScout has to be built at least once in release mode, as the
-# build/linux-release directory is required. The directory is assumed to reside next to this script
+# build/linux-Release directory is required. The directory is assumed to reside next to this script
 # or one level above.
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 # Run clang-tidy on all source files
-if [ -d "$SCRIPT_DIR/build/linux-release" ] 
+if [ -d "$SCRIPT_DIR/build/linux-Release" ] 
 then
-  run-clang-tidy -fix -quiet -p "$SCRIPT_DIR/build/linux-release"
-elif [ -d "$SCRIPT_DIR/../build/linux-release" ] 
+  run-clang-tidy -fix -quiet -p "$SCRIPT_DIR/build/linux-Release"
+elif [ -d "$SCRIPT_DIR/../build/linux-Release" ] 
 then
-  run-clang-tidy -fix -quiet -p "$SCRIPT_DIR/../build/linux-release"
+  run-clang-tidy -fix -quiet -p "$SCRIPT_DIR/../build/linux-Release"
 else
     echo "Failed to find build directory!"
 fi
