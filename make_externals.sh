d6c9b7252ea8d7da1c916e5a4c822ed9a992baae
Hannes <hannes@octofox.de>
2020-10-30 13:44:34 +0100

    :wrench: Add zipper

--- make_externals.sh
+++ make_externals.sh
@@ -67,146 +67,159 @@ cmake -E make_directory "$INSTALL_DIR/share"
 cmake -E make_directory "$INSTALL_DIR/bin"
 cmake -E make_directory "$INSTALL_DIR/include"
 
-
-# Proj6 ---------------------------------------------------------------------------------------------
+# Zipper ---------------------------------------------------------------------------------------------
 
 echo ""
-echo "Downloading, building and installing PROJ6 ..."
+echo "Building and installing zipper ..."
 echo ""
 
-# SQLITE Binary
-cd "$BUILD_DIR"
-wget -nc https://github.com/boramalper/sqlite3-x64/releases/download/3310100--2020-02-18T12.16.42Z/sqlite3
-chmod +x "$BUILD_DIR/sqlite3"
-
-cmake -E make_directory "$BUILD_DIR/proj6/extracted" && cd "$BUILD_DIR/proj6"
-wget -nc https://download.osgeo.org/proj/proj-6.3.2.tar.gz
-
-cd "$BUILD_DIR/proj6/extracted"
-cmake -E tar xzf ../proj-6.3.2.tar.gz
-cd "$BUILD_DIR/proj6/extracted/proj-6.3.2"
-
-#-DBUILD_CCT=OFF -DBUILD_CS2CS=OFF -DBUILD_GEOD=OFF -DBUILD_GIE=OFF -DBUILD_PROJINFO=OFF -DBUILD_PROJSYNC=OFF -DENABLE_CURL=OFF -DENABLE_TIFF=OFF \ 
-#-DSQLITE3_LIBRARY=/usr/lib/x86_64-linux-gnu/libsqlite3.so \
-
+cmake -E make_directory "$BUILD_DIR/zipper" && cd "$BUILD_DIR/zipper"
 cmake "${CMAKE_FLAGS[@]}" -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
-      -DCMAKE_INSTALL_LIBDIR=lib \
-      -DPROJ_TESTS=OFF \
-      -DEXE_SQLITE3="$BUILD_DIR/sqlite3" \
-      -DSQLITE3_INCLUDE_DIR="$EXTERNALS_DIR/sqlite3" \
-      -DCMAKE_BUILD_TYPE="$BUILD_TYPE" "$BUILD_DIR/proj6/extracted/proj-6.3.2"
+      -DCMAKE_BUILD_TYPE=$BUILD_TYPE "$EXTERNALS_DIR/zipper"
 cmake --build . --target install --parallel "$(nproc)"
 
+# Compile GDAL From source ----------------------------------------------------------------------------
+# Very much WIP
+if [ "$COSMOSCOUT_NO_SYSTEM_GDAL" = true ] then
+  # Proj6 ---------------------------------------------------------------------------------------------
 
-# LibJPEG ---------------------------------------------------------------------------------------------
+  echo ""
+  echo "Downloading, building and installing PROJ6 ..."
+  echo ""
 
-echo ""
-echo "Downloading, building and installing libjpeg ..."
-echo ""
+  # SQLITE Binary
+  cd "$BUILD_DIR"
+  wget -nc https://github.com/boramalper/sqlite3-x64/releases/download/3310100--2020-02-18T12.16.42Z/sqlite3
+  chmod +x "$BUILD_DIR/sqlite3"
 
-cmake -E make_directory "$BUILD_DIR/libjpeg/extracted" && cd "$BUILD_DIR/libjpeg"
-wget -nc https://github.com/libjpeg-turbo/libjpeg-turbo/archive/2.0.5.tar.gz
+  cmake -E make_directory "$BUILD_DIR/proj6/extracted" && cd "$BUILD_DIR/proj6"
+  wget -nc https://download.osgeo.org/proj/proj-6.3.2.tar.gz
 
-cd "$BUILD_DIR/libjpeg/extracted"
-cmake -E tar xzf ../2.0.5.tar.gz
-cd "$BUILD_DIR/libjpeg/extracted/libjpeg-turbo-2.0.5"
+  cd "$BUILD_DIR/proj6/extracted"
+  cmake -E tar xzf ../proj-6.3.2.tar.gz
+  cd "$BUILD_DIR/proj6/extracted/proj-6.3.2"
 
-cmake "${CMAKE_FLAGS[@]}" -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
-      -DCMAKE_INSTALL_LIBDIR=lib \
-      -DCMAKE_BUILD_TYPE="$BUILD_TYPE" "$BUILD_DIR/libjpeg/extracted/libjpeg-turbo-2.0.5"
-cmake --build . --target install --parallel "$(nproc)"
+  #-DBUILD_CCT=OFF -DBUILD_CS2CS=OFF -DBUILD_GEOD=OFF -DBUILD_GIE=OFF -DBUILD_PROJINFO=OFF -DBUILD_PROJSYNC=OFF -DENABLE_CURL=OFF -DENABLE_TIFF=OFF \
+  #-DSQLITE3_LIBRARY=/usr/lib/x86_64-linux-gnu/libsqlite3.so \
 
-
-# LibPNG ---------------------------------------------------------------------------------------------
-
-echo ""
-echo "Downloading, building and installing libpng ..."
-echo ""
-
-cmake -E make_directory "$BUILD_DIR/libpng/extracted" && cd "$BUILD_DIR/libpng"
-wget -nc https://downloads.sourceforge.net/project/libpng/libpng16/1.6.37/libpng-1.6.37.tar.gz
-
-cd "$BUILD_DIR/libpng/extracted"
-cmake -E tar xzf ../libpng-1.6.37.tar.gz
-cd "$BUILD_DIR/libpng/extracted/libpng-1.6.37"
-
-cmake "${CMAKE_FLAGS[@]}" -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
-      -DCMAKE_INSTALL_LIBDIR=lib \
-      -DCMAKE_BUILD_TYPE="$BUILD_TYPE" "$BUILD_DIR/libpng/extracted/libpng-1.6.37"
-cmake --build . --target install --parallel "$(nproc)"
+  cmake "${CMAKE_FLAGS[@]}" -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
+        -DCMAKE_INSTALL_LIBDIR=lib \
+        -DPROJ_TESTS=OFF \
+        -DEXE_SQLITE3="$BUILD_DIR/sqlite3" \
+        -DSQLITE3_INCLUDE_DIR="$EXTERNALS_DIR/sqlite3" \
+        -DCMAKE_BUILD_TYPE="$BUILD_TYPE" "$BUILD_DIR/proj6/extracted/proj-6.3.2"
+  cmake --build . --target install --parallel "$(nproc)"
 
 
-# gdal 3.0.4 --------------------------------------------------------------------------------------------
+  # LibJPEG ---------------------------------------------------------------------------------------------
 
-echo ""
-echo "Downloading and installing gdal ..."
-echo ""
+  echo ""
+  echo "Downloading, building and installing libjpeg ..."
+  echo ""
 
-cmake -E make_directory "$BUILD_DIR/gdal/extracted" && cd "$BUILD_DIR/gdal"
-wget -nc https://github.com/OSGeo/gdal/releases/download/v3.0.4/gdal-3.0.4.tar.gz
+  cmake -E make_directory "$BUILD_DIR/libjpeg/extracted" && cd "$BUILD_DIR/libjpeg"
+  wget -nc https://github.com/libjpeg-turbo/libjpeg-turbo/archive/2.0.5.tar.gz
 
-cd "$BUILD_DIR/gdal/extracted"
-cmake -E tar xzf ../gdal-3.0.4.tar.gz
-cd "$BUILD_DIR/gdal/extracted/gdal-3.0.4"
+  cd "$BUILD_DIR/libjpeg/extracted"
+  cmake -E tar xzf ../2.0.5.tar.gz
+  cd "$BUILD_DIR/libjpeg/extracted/libjpeg-turbo-2.0.5"
 
-./configure --prefix="$INSTALL_DIR/gdal" \
-    --with-proj="$INSTALL_DIR" \
-    --with-jpeg="$INSTALL_DIR" \
-    --with-png="$INSTALL_DIR" \
-    --with-geos \
-    --with-geotiff=internal \
-    --with-hide-internal-symbols \
-    --with-libtiff=internal \
-    --with-libz=internal \
-    --with-threads \
-    --without-cfitsio \
-    --without-cryptopp \
-    --without-curl \
-    --without-ecw \
-    --without-expat \
-    --without-fme \
-    --without-frmts \
-    --without-freexl \
-    --without-gif \
-    --without-gif \
-    --without-gnm \
-    --without-grass \
-    --without-hdf4 \
-    --without-hdf5 \
-    --without-idb \
-    --without-ingres \
-    --without-jasper \
-    --without-jp2mrsid \
-    --without-kakadu \
-    --without-libgrass \
-    --without-libkml \
-    --without-libtool \
-    --without-mrsid \
-    --without-mysql \
-    --without-netcdf \
-    --without-odbc \
-    --without-ogdi \
-    --without-openjpeg \
-    --without-pcidsk \
-    --without-pcraster \
-    --without-pcre \
-    --without-perl \
-    --without-pg \
-    --without-python \
-    --without-qhull \
-    --without-sde \
-    --without-sqlite3 \
-    --without-webp \
-    --without-xerces \
-    --without-xml2
-make
-make install
+  cmake "${CMAKE_FLAGS[@]}" -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
+        -DCMAKE_INSTALL_LIBDIR=lib \
+        -DCMAKE_BUILD_TYPE="$BUILD_TYPE" "$BUILD_DIR/libjpeg/extracted/libjpeg-turbo-2.0.5"
+  cmake --build . --target install --parallel "$(nproc)"
 
-#cmake "${CMAKE_FLAGS[@]}" -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
-#      -DCMAKE_INSTALL_LIBDIR=lib \
-#      -DCMAKE_BUILD_TYPE=$BUILD_TYPE "$BUILD_DIR/gdal/extracted/lib_gdal-3.0.3"
-#cmake --build . --target install --parallel "$(nproc)"
 
+  # LibPNG ---------------------------------------------------------------------------------------------
+
+  echo ""
+  echo "Downloading, building and installing libpng ..."
+  echo ""
+
+  cmake -E make_directory "$BUILD_DIR/libpng/extracted" && cd "$BUILD_DIR/libpng"
+  wget -nc https://downloads.sourceforge.net/project/libpng/libpng16/1.6.37/libpng-1.6.37.tar.gz
+
+  cd "$BUILD_DIR/libpng/extracted"
+  cmake -E tar xzf ../libpng-1.6.37.tar.gz
+  cd "$BUILD_DIR/libpng/extracted/libpng-1.6.37"
+
+  cmake "${CMAKE_FLAGS[@]}" -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
+        -DCMAKE_INSTALL_LIBDIR=lib \
+        -DCMAKE_BUILD_TYPE="$BUILD_TYPE" "$BUILD_DIR/libpng/extracted/libpng-1.6.37"
+  cmake --build . --target install --parallel "$(nproc)"
+
+
+  # gdal 3.0.4 --------------------------------------------------------------------------------------------
+
+  echo ""
+  echo "Downloading and installing gdal ..."
+  echo ""
+
+  cmake -E make_directory "$BUILD_DIR/gdal/extracted" && cd "$BUILD_DIR/gdal"
+  wget -nc https://github.com/OSGeo/gdal/releases/download/v3.0.4/gdal-3.0.4.tar.gz
+
+  cd "$BUILD_DIR/gdal/extracted"
+  cmake -E tar xzf ../gdal-3.0.4.tar.gz
+  cd "$BUILD_DIR/gdal/extracted/gdal-3.0.4"
+
+  ./configure --prefix="$INSTALL_DIR/gdal" \
+      --with-proj="$INSTALL_DIR" \
+      --with-jpeg="$INSTALL_DIR" \
+      --with-png="$INSTALL_DIR" \
+      --with-geos \
+      --with-geotiff=internal \
+      --with-hide-internal-symbols \
+      --with-libtiff=internal \
+      --with-libz=internal \
+      --with-threads \
+      --without-cfitsio \
+      --without-cryptopp \
+      --without-curl \
+      --without-ecw \
+      --without-expat \
+      --without-fme \
+      --without-frmts \
+      --without-freexl \
+      --without-gif \
+      --without-gif \
+      --without-gnm \
+      --without-grass \
+      --without-hdf4 \
+      --without-hdf5 \
+      --without-idb \
+      --without-ingres \
+      --without-jasper \
+      --without-jp2mrsid \
+      --without-kakadu \
+      --without-libgrass \
+      --without-libkml \
+      --without-libtool \
+      --without-mrsid \
+      --without-mysql \
+      --without-netcdf \
+      --without-odbc \
+      --without-ogdi \
+      --without-openjpeg \
+      --without-pcidsk \
+      --without-pcraster \
+      --without-pcre \
+      --without-perl \
+      --without-pg \
+      --without-python \
+      --without-qhull \
+      --without-sde \
+      --without-sqlite3 \
+      --without-webp \
+      --without-xerces \
+      --without-xml2
+  make
+  make install
+
+  #cmake "${CMAKE_FLAGS[@]}" -DCMAKE_INSTALL_PREFIX="$INSTALL_DIR" \
+  #      -DCMAKE_INSTALL_LIBDIR=lib \
+  #      -DCMAKE_BUILD_TYPE=$BUILD_TYPE "$BUILD_DIR/gdal/extracted/lib_gdal-3.0.3"
+  #cmake --build . --target install --parallel "$(nproc)"
+fi
 
 # VTK -----------------------------------------------------------------------------------------
 
