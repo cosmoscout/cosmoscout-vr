30c7f88c6dde4077be33cc1691e6c3661c7c3c98
Jonathan Fritsch <jonathan.fritsch@dlr.de>
2020-12-08 10:29:06 +0100

    :truck: Move transfer function editor from volume rendering plugin to core

--- src/cosmoscout/Application.hpp
+++ src/cosmoscout/Application.hpp
@@ -164,6 +164,9 @@ class Application : public VistaFrameLoop {
   /// "time.set"
   /// "time.setDate"
   /// "time.setSpeed"
+  /// "transferFunctionEditor.importTransferFunction"
+  /// "transferFunctionEditor.exportTransferFunction"
+  /// "transferFunctionEditor.getAvailableTransferFunctions"
   void registerGuiCallbacks();
   void unregisterGuiCallbacks();
 
