0e339691a3ab37d8bd7b2117402abe4cf38cf2d4
jonas <jonas.gilg@stud.hs-hannover.de>
2021-04-01 01:14:11 +0200

    :sparkles: Run clang format.

--- plugins/csp-guide/gui/js/csp-guide.js
+++ plugins/csp-guide/gui/js/csp-guide.js
@@ -15,18 +15,16 @@
     #clippyApi = null;
     #lastState = null;
 
-    #clippyWidth = 0;
+    #clippyWidth  = 0;
     #clippyHeight = 0;
 
     /**
      * @inheritDoc
      */
     init() {
-      import("./third-party/js/clippyjs/index.js").then(m => {
-        this.#clippyApi = m.default;
-        new this.#clippyApi.load('Clippy', (agent) => {
-          this.agent = agent;
-        }, undefined, './third-party/agents/');
+import("./third-party/js/clippyjs/index.js").then(m => {
+        this.#clippyApi = m.default; new this.#clippyApi.load(
+            'Clippy', (agent) => { this.agent = agent;}, undefined, './third-party/agents/');
       });
 
       this.#lastState = deepCopy(CosmoScout.state);
@@ -45,21 +43,22 @@
       this.#lastState = deepCopy(CosmoScout.state);
     }
 
-    #hasGreeted = false;
+    #hasGreeted         = false;
     #hasGreetedFinished = false;
     #greet() {
       if (!this.#hasGreeted) {
         this.#hasGreeted = true;
         this.agent.show();
         this.agent.play("Greeting");
-        this.agent.speak("Hi, I am your guide, Clippy. I am here to help you navigate this large Universe.");
+        this.agent.speak(
+            "Hi, I am your guide, Clippy. I am here to help you navigate this large Universe.");
         this.agent.play("IdleHeadScratch");
         this.agent.speak("I will also help you navigate the user interface, of course.");
         this.agent.play("Wave");
 
         this.agent.onEmptyQueue(() => {
           this.#hasGreetedFinished = true;
-          this.#clippyWidth = document.querySelector(".clippy").getBoundingClientRect().width;
+          this.#clippyWidth  = document.querySelector(".clippy").getBoundingClientRect().width;
           this.#clippyHeight = document.querySelector(".clippy").getBoundingClientRect().height;
         });
       }
@@ -75,13 +74,17 @@
           this.#centerChangeDisplayed = true;
 
           const bookmarksTabElement = document.querySelector("#heading-sidebar-tab-Bookmarks");
-          const bookmarksRect = bookmarksTabElement.getBoundingClientRect();
+          const bookmarksRect       = bookmarksTabElement.getBoundingClientRect();
 
           this.agent.play("GetAttention");
-          this.agent.moveTo(bookmarksRect.right + 50, (bookmarksRect.top + bookmarksRect.bottom - this.#clippyHeight) / 2);
-          this.agent.gestureAt(bookmarksRect.left, (bookmarksRect.top + bookmarksRect.bottom - this.#clippyHeight) / 2);
-          this.agent.speak("Hi, I see you moved away from a planet. You can use the bookmarks tab to move to places. This way you won't get lost in the infinite space of the Universe!");
-          this.agent.gestureAt(bookmarksRect.left, (bookmarksRect.top + bookmarksRect.bottom - this.#clippyHeight) / 2);
+          this.agent.moveTo(bookmarksRect.right + 50,
+              (bookmarksRect.top + bookmarksRect.bottom - this.#clippyHeight) / 2);
+          this.agent.gestureAt(bookmarksRect.left,
+              (bookmarksRect.top + bookmarksRect.bottom - this.#clippyHeight) / 2);
+          this.agent.speak(
+              "Hi, I see you moved away from a planet. You can use the bookmarks tab to move to places. This way you won't get lost in the infinite space of the Universe!");
+          this.agent.gestureAt(bookmarksRect.left,
+              (bookmarksRect.top + bookmarksRect.bottom - this.#clippyHeight) / 2);
           this.agent.moveTo(bookmarksRect.right + 350, bookmarksRect.top);
 
           this.agent.onEmptyQueue(() => {
@@ -96,7 +99,7 @@
     #planetRotated = false;
     #planetRotateChecker() {
       function length2(vector) {
-        return vector[0]**2 + vector[1]**2 + vector[2]**2;
+        return vector[0] ** 2 + vector[1] ** 2 + vector[2] ** 2;
       }
 
       function isSame(a, b) {
@@ -106,36 +109,47 @@
       const prevPos = this.#lastState.observerPosition;
       const currPos = CosmoScout.state.observerPosition;
 
-      if (!this.#planetRotated && !isSame(prevPos, currPos) && length2(prevPos) !== length2(currPos)) {
+      if (!this.#planetRotated && !isSame(prevPos, currPos) &&
+          length2(prevPos) !== length2(currPos)) {
         this.#planetRotated = true;
 
         this.agent.play("GetAttention");
-        this.agent.speak("I see you rotated the planet! You can interact in different ways with the planet.");
+        this.agent.speak(
+            "I see you rotated the planet! You can interact in different ways with the planet.");
         {
-          const turnNorthUpButton = document.querySelector("#turn-north-up-button");
+          const turnNorthUpButton     = document.querySelector("#turn-north-up-button");
           const turnNorthUpButtonRect = turnNorthUpButton.getBoundingClientRect();
           this.agent.moveTo(turnNorthUpButtonRect.left, turnNorthUpButtonRect.bottom);
           this.agent.play("LookUpRight");
-          this.agent.speak("You can click the button with the compass to orient the camera so north is the up direction.");
+          this.agent.speak(
+              "You can click the button with the compass to orient the camera so north is the up direction.");
         }
         {
-          const fixHorizonButton = document.querySelector("#fix-horizon-button");
+          const fixHorizonButton     = document.querySelector("#fix-horizon-button");
           const fixHorizonButtonRect = fixHorizonButton.getBoundingClientRect();
-          this.agent.moveTo((fixHorizonButtonRect.left + fixHorizonButtonRect.right - this.#clippyWidth) / 2, fixHorizonButtonRect.bottom);
+          this.agent.moveTo(
+              (fixHorizonButtonRect.left + fixHorizonButtonRect.right - this.#clippyWidth) / 2,
+              fixHorizonButtonRect.bottom);
           this.agent.play("GestureUp");
           this.agent.speak("You can click this button to look at the horizon.");
         }
         {
-          const landOnSurfaceButton = document.querySelector("#land-on-surface-button");
+          const landOnSurfaceButton     = document.querySelector("#land-on-surface-button");
           const landOnSurfaceButtonRect = landOnSurfaceButton.getBoundingClientRect();
-          this.agent.moveTo((landOnSurfaceButtonRect.left + landOnSurfaceButtonRect.right - this.#clippyWidth) / 2, landOnSurfaceButtonRect.bottom);
+          this.agent.moveTo(
+              (landOnSurfaceButtonRect.left + landOnSurfaceButtonRect.right - this.#clippyWidth) /
+                  2,
+              landOnSurfaceButtonRect.bottom);
           this.agent.play("GestureUp");
           this.agent.speak("You can click this button to land on the surface.");
         }
         {
-          const launchToOrbitButton = document.querySelector("#launch-to-orbit-button");
+          const launchToOrbitButton     = document.querySelector("#launch-to-orbit-button");
           const launchToOrbitButtonRect = launchToOrbitButton.getBoundingClientRect();
-          this.agent.moveTo((launchToOrbitButtonRect.left + launchToOrbitButtonRect.right - this.#clippyWidth) / 2, launchToOrbitButtonRect.bottom);
+          this.agent.moveTo(
+              (launchToOrbitButtonRect.left + launchToOrbitButtonRect.right - this.#clippyWidth) /
+                  2,
+              launchToOrbitButtonRect.bottom);
           this.agent.play("GestureUp");
           this.agent.speak("You can click this button to fly back into orbit.");
         }
