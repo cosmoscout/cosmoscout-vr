0a13dc6f57769c9f7926719e87686d35943d7af9
Simon Schneegans <code@simonschneegans.de>
2022-03-14 12:36:30 +0100

    :wrench: Add reference eclipse modes

--- resources/shaders/eclipseShadows.glsl
+++ resources/shaders/eclipseShadows.glsl
@@ -117,109 +117,6 @@ vec4 _eclipseGetBodyDirAngle(vec4 body, vec3 position) {
   return vec4(bodyDir, bodyAngle);
 }
 
-vec3 _eclipseShadowMapLookup(int i, vec3 position) {
-  float sunDistance = length(uEclipseSun.xyz - uEclipseOccluders[i].xyz);
-  float sunRadius   = uEclipseSun.w;
-
-  float rOcc   = uEclipseOccluders[i].w;
-  float dOcc   = sunDistance / (sunRadius / rOcc + 1);
-  float y0     = rOcc / dOcc * sqrt(dOcc * dOcc - rOcc * rOcc);
-  float xOcc   = sqrt(rOcc * rOcc - y0 * y0);
-  float xUmbra = (sunDistance * rOcc) / (sunRadius - rOcc);
-  float xF     = xOcc - dOcc;
-  float fac    = y0 / -xF;
-
-  vec3 pSunOcc = _eclipseProjectPointOnRay(
-      uEclipseOccluders[i].xyz, uEclipseOccluders[i].xyz - uEclipseSun.xyz, position);
-  vec2 pos = vec2(distance(pSunOcc, uEclipseOccluders[i].xyz), distance(pSunOcc, position));
-
-  // Todo: Maybe pos.x += xOcc is required?
-
-  float alphaX = pos.x / (pos.x + xUmbra);
-  float alphaY = pos.y / (fac * (pos.x - xF));
-
-  float x = pow(alphaX, 1.0 / ECLIPSE_TEX_SHADOW_EXPONENT);
-  float y = alphaY;
-
-  if (x <= 0.0 || x >= 1.0 || y <= 0.0 || y >= 1.0) {
-    return vec3(1.0);
-  }
-
-  return texture(uEclipseShadowMaps[i], vec2(x, 1 - y)).rgb;
-}
-
-// https://github.com/OpenSpace/OpenSpace/blob/master/modules/globebrowsing/shaders/renderer_fs.glsl#L115
-vec3 _eclipseGetOpenSpaceParams(int i, vec3 position) {
-  float sunDistance = length(uEclipseSun.xyz - uEclipseOccluders[i].xyz);
-  float sunRadius   = uEclipseSun.w;
-  float rOcc        = uEclipseOccluders[i].w;
-
-  vec3 pc      = uEclipseOccluders[i].xyz - position;
-  vec3 sc_norm = (uEclipseOccluders[i].xyz - uEclipseSun.xyz) / sunDistance;
-  vec3 pc_proj = dot(pc, sc_norm) * sc_norm;
-  vec3 d       = pc - pc_proj;
-
-  float length_d       = length(d);
-  float length_pc_proj = length(pc_proj);
-
-  float xp = sunDistance / (sunRadius + rOcc);
-  float xu = rOcc * sunDistance / (sunRadius - rOcc);
-
-  float r_p_pi = rOcc * (length_pc_proj + xp) / xp;
-  float r_u_pi = rOcc * (xu - length_pc_proj) / xu;
-
-  return vec3(length_d, r_u_pi, r_p_pi);
-}
-
-vec3 _eclipseOpenSpace(int i, vec3 position) {
-  vec3 params = _eclipseGetOpenSpaceParams(i, position);
-
-  if (params.x < params.y) { // umbra
-    // return vec3(0.0);
-    return vec3(sqrt(params.y / (params.y + pow(params.x, 2.0))));
-  } else if (params.x < params.z) { // penumbra
-    // return vec3((params.x - params.y) / params.z);
-    return vec3(params.x / params.z);
-  }
-
-  return vec3(1.0);
-}
-
-// https://github.com/CelestiaProject/Celestia/blob/master/src/celengine/shadermanager.cpp#L3811
-// https://github.com/CelestiaProject/Celestia/blob/master/src/celengine/render.cpp#L2969
-// https://github.com/CelestiaProject/Celestia/blob/master/src/celengine/shadermanager.cpp#L1344
-vec3 _eclipseCelestia(int i, vec3 position) {
-  // float distToSun    = length(uEclipseSun.xyz - position);
-  // float appSunRadius = uEclipseSun.w / distToSun;
-
-  // float distToCaster      = length(uEclipseOccluders[i].xyz - position);
-  // float appOccluderRadius = uEclipseOccluders[i].w / distToCaster;
-
-  // float penumbraRadius = (1 + appSunRadius / appOccluderRadius) * uEclipseOccluders[i].w;
-
-  // float umbraRadius =
-  //     uEclipseOccluders[i].w * (appOccluderRadius - appSunRadius) / appOccluderRadius;
-  // float maxDepth = min(1.0, pow(appOccluderRadius / appSunRadius, 2.0));
-
-  // float umbra   = umbraRadius / penumbraRadius;
-  // float falloff = -maxDepth / max(0.001, 1.0 - abs(umbra));
-
-  // vec3 params = _eclipseGetOpenSpaceParams(i, position);
-  // float shadowR = clamp((2.0 * params.x - 1.0) * falloff, 0.0, maxDepth);
-
-  // return vec3(1.0 - shadowR);
-
-  vec3 params = _eclipseGetOpenSpaceParams(i, position);
-
-  if (params.x < params.y) { // umbra
-    return vec3(0.0);
-  } else if (params.x < params.z) { // penumbra
-    return vec3((params.x - params.y) / (params.z - params.y));
-  }
-
-  return vec3(1.0);
-}
-
 vec3 getEclipseShadow(vec3 position) {
 
   // None.
@@ -227,7 +124,10 @@ vec3 getEclipseShadow(vec3 position) {
     return vec3(1.0);
   }
 
-  // Debug.
+  // -----------------------------------------------------------------------------------------------
+  // ------------------------------------- Debug Mode ----------------------------------------------
+  // -----------------------------------------------------------------------------------------------
+
   if (uEclipseMode == 1) {
     vec3 light = vec3(1.0);
 
@@ -239,46 +139,165 @@ vec3 getEclipseShadow(vec3 position) {
       float sunBodyDist  = _eclipseGetAngle(sunDirAngle.xyz, bodyDirAngle.xyz);
 
       if (sunDirAngle.w < bodyDirAngle.w - sunBodyDist) { // Total eclipse.
-        light *= vec3(0.25);
+        light *= vec3(1.0, 0.5, 0.5);
+      } else if (sunBodyDist < sunDirAngle.w - bodyDirAngle.w) { // Annular eclipse.
+        light *= vec3(0.5, 1.0, 0.5);
       } else if (sunBodyDist < sunDirAngle.w + bodyDirAngle.w) { // Partial eclipse.
-        light *= vec3(0.75);
+        light *= vec3(0.5, 0.5, 1.0);
       }
     }
 
     return light;
   }
 
-  // Celestia.
+  // -----------------------------------------------------------------------------------------------
+  // -------------------------------------- Celestia -----------------------------------------------
+  // -----------------------------------------------------------------------------------------------
+
+  // Quaoting a source code comment from Celestia: "All of the eclipse related code assumes that
+  // both the caster and receiver are spherical. Irregular receivers will work more or less
+  // correctly, but casters that are sufficiently non-spherical will produce obviously incorrect
+  // shadows. Another assumption we make is that the distance between the caster and receiver is
+  // much less than the distance between the sun and the receiver. This approximation works
+  // everywhere in the solar system, and is likely valid for any orbitally stable pair of objects
+  // orbiting a star."
+
+  // Also from the source code: "The shadow shadow consists of a circular region of constant depth
+  // (maxDepth), surrounded by a ring of linear falloff from maxDepth to zero. For a total eclipse,
+  // maxDepth is zero. In reality, the falloff function is much more complex: to calculate the exact
+  // amount of sunlight blocked, we need to calculate the a circle-circle intersection area."
+
+  // Based on this code:
+  // https://github.com/CelestiaProject/Celestia/blob/master/src/celengine/shadermanager.cpp#L1344
+  // https://github.com/CelestiaProject/Celestia/blob/master/src/celengine/shadermanager.cpp#L3811
+  // https://github.com/CelestiaProject/Celestia/blob/master/src/celengine/render.cpp#L2969
+
   if (uEclipseMode == 2) {
     vec3 light = vec3(1.0);
     for (int i = 0; i < uEclipseNumOccluders; ++i) {
-      light *= _eclipseCelestia(i, position);
+      float distToSun    = length(uEclipseOccluders[i].xyz - uEclipseSun.xyz);
+      float appSunRadius = uEclipseSun.w / distToSun;
+
+      float distToCaster      = length(uEclipseOccluders[i].xyz - position);
+      float appOccluderRadius = uEclipseOccluders[i].w / distToCaster;
+
+      float penumbraRadius = (1 + appSunRadius / appOccluderRadius) * uEclipseOccluders[i].w;
+
+      float umbraRadius =
+          uEclipseOccluders[i].w * (appOccluderRadius - appSunRadius) / appOccluderRadius;
+      float maxDepth = min(1.0, pow(appOccluderRadius / appSunRadius, 2.0));
+
+      float umbra   = umbraRadius / penumbraRadius;
+      float falloff = maxDepth / max(0.001, 1.0 - abs(umbra));
+
+      // Compute distance from fragment to Sun-Occluder ray.
+      vec3  pc       = uEclipseOccluders[i].xyz - position;
+      vec3  sc_norm  = (uEclipseOccluders[i].xyz - uEclipseSun.xyz) / distToSun;
+      vec3  pc_proj  = dot(pc, sc_norm) * sc_norm;
+      float length_d = length(pc - pc_proj);
+
+      // This r is computed quite differently in Celestia. This is due to the fact that eclipse
+      // shadows are not computed in worldsapce in Celestia but rather in a shadow-local coordinate
+      // system.
+      float r = 1 - length_d / penumbraRadius;
+
+      if (r < 1.0) {
+        float shadowR = clamp(r * falloff, 0.0, maxDepth);
+        light *= 1 - shadowR;
+      }
     }
 
     return light;
   }
 
-  // Cosmographia.
+  // -----------------------------------------------------------------------------------------------
+  // ----------------------------------- Cosmographia ----------------------------------------------
+  // -----------------------------------------------------------------------------------------------
+
+  // Based on this code:
+  // https://github.com/claurel/cosmographia/blob/master/thirdparty/vesta/internal/EclipseShadowVolumeSet.cpp
+  // https://github.com/claurel/cosmographia/blob/master/thirdparty/vesta/ShaderBuilder.cpp#L222
+
   if (uEclipseMode == 3) {
     vec3 light = vec3(1.0);
-    for (int i = 0; i < uEclipseNumOccluders; ++i) {
-      light *= _eclipseCelestia(i, position);
-    }
+    for (int i = 0; i < uEclipseNumOccluders; ++i) {}
 
     return light;
   }
 
-  // OpenSpace.
+  // -----------------------------------------------------------------------------------------------
+  // ------------------------------------- OpenSpace -----------------------------------------------
+  // -----------------------------------------------------------------------------------------------
+
+  // At the moment, the eclipse shadows in OpenSpace seem to be quite basic. They assume a spherical
+  // light source as well as spherical shadow casters. There are some geometric simplifications in
+  // computing the umbra and penumbra cones - effectively the Sun and the shadow caster are modelled
+  // as circles oriented perpendicular to the Sun-Occluder axis. Furthermore, there seems to be no
+  // shadow falloff beyond the end of the umbra. The penumbra keeps getting wider and wider, but the
+  // center of the shadow volume will always stay black.
+
+  // Based on this code:
+  // https://github.com/OpenSpace/OpenSpace/blob/d7d279ea168f5eaa6a0109593360774246699c4e/modules/globebrowsing/shaders/renderer_fs.glsl#L93
+  // https://github.com/OpenSpace/OpenSpace/blob/d7d279ea168f5eaa6a0109593360774246699c4e/modules/globebrowsing/src/renderableglobe.cpp#L2086
+
   if (uEclipseMode == 4) {
+
     vec3 light = vec3(1.0);
+
     for (int i = 0; i < uEclipseNumOccluders; ++i) {
-      light *= _eclipseOpenSpace(i, position);
+      float sunDistance = length(uEclipseSun.xyz - uEclipseOccluders[i].xyz);
+
+      // Project the vector from fragment to occluder on the Sun-Occluder ray.
+      vec3  pc             = uEclipseOccluders[i].xyz - position;
+      vec3  sc_norm        = (uEclipseOccluders[i].xyz - uEclipseSun.xyz) / sunDistance;
+      vec3  pc_proj        = dot(pc, sc_norm) * sc_norm;
+      float length_pc_proj = length(pc_proj);
+
+      // Compute distance from fragment to Sun-Occluder ray.
+      vec3  d        = pc - pc_proj;
+      float length_d = length(d);
+
+      // Compute focus point of the penumbra cone. Somewhere in front of the occluder.
+      float xp = uEclipseOccluders[i].w * sunDistance / (uEclipseSun.w + uEclipseOccluders[i].w);
+
+      // Compute focus point of the umbra cone. Somewhere behind occluder.
+      float xu = uEclipseOccluders[i].w * sunDistance / (uEclipseSun.w - uEclipseOccluders[i].w);
+
+      // The radius of the penumbra cone, computed with the intercept theorem. This is not really
+      // correct, as the tangents at the occluder do not really touch the poles.
+      float r_p_pi = uEclipseOccluders[i].w * (length_pc_proj + xp) / xp;
+      float r_u_pi = uEclipseOccluders[i].w * (xu - length_pc_proj) / xu;
+
+      if (length_d < r_u_pi) { // umbra
+
+        // The original code uses this:
+        // light *= sqrt(r_u_pi / (r_u_pi + pow(length_d, 2.0)));
+
+        // In open space, this is close to zero in most cases, however as we are in the umbra, using
+        // exaclty zero seems more correct...
+        light *= 0.0;
+
+      } else if (length_d < r_p_pi) { // penumbra
+
+        // This returns a linear falloff from the center of the shadow to the penumbra's edge. Using
+        // light *= (length_d - max(0, r_u_pi)) / (r_p_pi - max(0, r_u_pi));
+        // would have been better as this decays to zero towards the umbra. Nevertheless, this code
+        // still returns a completely black shadow center even behind the end of the umbra...?
+
+        light *= length_d / r_p_pi;
+      }
     }
 
     return light;
   }
 
-  // Circle Intersection, Approximated Spherical Cap Intersection, or Spherical Cap Intersection.
+  // -----------------------------------------------------------------------------------------------
+  // ---------------------------- Various Analytical Approaches ------------------------------------
+  // -----------------------------------------------------------------------------------------------
+
+  // 6: Circle Intersection
+  // 7: Approximated Spherical Cap Intersection
+  // 8: Spherical Cap Intersection
   if (uEclipseMode == 5 || uEclipseMode == 6 || uEclipseMode == 7) {
 
     vec3 light = vec3(1.0);
@@ -307,10 +326,39 @@ vec3 getEclipseShadow(vec3 position) {
     return light;
   }
 
-  // Texture Lookups.
+  // -----------------------------------------------------------------------------------------------
+  // ------------------ Various Approaches based on Texture Lookups --------------------------------
+  // -----------------------------------------------------------------------------------------------
+
   vec3 light = vec3(1.0);
+
   for (int i = 0; i < uEclipseNumOccluders; ++i) {
-    light *= _eclipseShadowMapLookup(i, position);
+
+    float sunDistance = length(uEclipseSun.xyz - uEclipseOccluders[i].xyz);
+
+    float rOcc   = uEclipseOccluders[i].w;
+    float dOcc   = sunDistance / (uEclipseSun.w / rOcc + 1);
+    float y0     = rOcc / dOcc * sqrt(dOcc * dOcc - rOcc * rOcc);
+    float xOcc   = sqrt(rOcc * rOcc - y0 * y0);
+    float xUmbra = (sunDistance * rOcc) / (uEclipseSun.w - rOcc);
+    float xF     = xOcc - dOcc;
+    float fac    = y0 / -xF;
+
+    vec3 pSunOcc = _eclipseProjectPointOnRay(
+        uEclipseOccluders[i].xyz, uEclipseOccluders[i].xyz - uEclipseSun.xyz, position);
+    vec2 pos = vec2(distance(pSunOcc, uEclipseOccluders[i].xyz), distance(pSunOcc, position));
+
+    // Todo: Maybe pos.x += xOcc is required?
+
+    float alphaX = pos.x / (pos.x + xUmbra);
+    float alphaY = pos.y / (fac * (pos.x - xF));
+
+    float x = pow(alphaX, 1.0 / ECLIPSE_TEX_SHADOW_EXPONENT);
+    float y = alphaY;
+
+    if (x > 0.0 && x < 1.0 && y > 0.0 && y < 1.0) {
+      light *= texture(uEclipseShadowMaps[i], vec2(x, 1 - y)).rgb;
+    }
   }
 
   return light;
