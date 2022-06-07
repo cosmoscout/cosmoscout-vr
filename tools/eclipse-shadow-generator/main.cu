c7d666c64a36127675a0629265ec478395be63bd
Simon Schneegans <code@simonschneegans.de>
2022-06-07 16:10:52 +0200

    :truck: Move error check to main

--- tools/eclipse-shadow-generator/main.cu
+++ tools/eclipse-shadow-generator/main.cu
@@ -7,13 +7,23 @@
 #include "../../src/cs-utils/CommandLine.hpp"
 
 #include "LimbDarkening.cuh"
-#include "cudaErrorCheck.hpp"
 #include "math.cuh"
 
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb_image.h>
 #include <stb_image_write.h>
+
+// https://stackoverflow.com/questions/14038589/what-is-the-canonical-way-to-check-for-errors-using-the-cuda-runtime-api
+#define gpuErrchk(ans)                                                                             \
+  { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char* file, int line, bool abort = true) {
+  if (code != cudaSuccess) {
+    fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+    if (abort)
+      exit(code);
+  }
+}
 
 struct ShadowSettings {
   uint32_t size            = 512;
