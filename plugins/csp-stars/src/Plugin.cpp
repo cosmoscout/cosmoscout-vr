c81f4162415fc8a5d07197e1b4eb027298c5009e
Simon Schneegans <simon.schneegans@dlr.de>
2022-08-22 09:07:52 +0200

    :wrench: Simplify matrix type conversion

--- plugins/csp-stars/src/Plugin.cpp
+++ plugins/csp-stars/src/Plugin.cpp
@@ -16,6 +16,7 @@
 #include <VistaKernel/GraphicsManager/VistaSceneGraph.h>
 #include <VistaKernel/GraphicsManager/VistaTransformNode.h>
 #include <VistaKernelOpenSGExt/VistaOpenSGMaterialTools.h>
+#include <glm/gtc/type_ptr.hpp>
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -245,9 +246,7 @@ void Plugin::update() {
 
   auto mat = mSolarSystem->getObject("Barycenter")->getObserverRelativeTransform();
 
-  mStarsTransform->SetTransform(VistaTransformMatrix(mat[0][0], mat[1][0], mat[2][0], mat[3][0],
-      mat[0][1], mat[1][1], mat[2][1], mat[3][1], mat[0][2], mat[1][2], mat[2][2], mat[3][2],
-      mat[0][3], mat[1][3], mat[2][3], mat[3][3]));
+  mStarsTransform->SetTransform(glm::value_ptr(mat), true);
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
