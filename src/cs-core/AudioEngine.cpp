92dc4597787d8cc253b62a4642411bc114fdbafd
florianSasz <100100037+florianSasz@users.noreply.github.com>
2023-09-27 21:57:43 +0200

    :wrench: adjust function param and add deconstructor

--- src/cs-core/AudioEngine.cpp
+++ src/cs-core/AudioEngine.cpp
@@ -15,13 +15,17 @@ namespace cs::core {
 
 AudioEngine::AudioEngine(std::shared_ptr<Settings> settings) 
     : mSettings(std::move(settings)) 
-    , mOpenAlManager(std::make_unique<audio::OpenAlManager>(mSettings->mAudio)) {
+    , mOpenAlManager(std::make_unique<audio::OpenAlManager>(mSettings)) {
 
   // Tell the user what's going on.
   logger().debug("Creating AudioEngine.");
   // logger().info("OpenAL-Soft Vendor:  {}", glGetString(GL_VENDOR));
   // logger().info("OpenAL-Soft Version: {}", glGetString(GL_VERSION));
   
+}
+
+AudioEngine::~AudioEngine() {
+
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
