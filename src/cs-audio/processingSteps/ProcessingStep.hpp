2e722f45916ab30ad03d0b9f6218276afb811973
Florian Sass <100100037+florianSasz@users.noreply.github.com>
2023-10-30 12:20:44 +0100

    :memo: Add documentation

--- src/cs-audio/processingSteps/ProcessingStep.hpp
+++ src/cs-audio/processingSteps/ProcessingStep.hpp
@@ -18,12 +18,19 @@ namespace cs::audio {
 
 class CS_AUDIO_EXPORT ProcessingStep {
  public:
-  // virtual std::shared_ptr<ProcessingStep> create() = 0;
+  
+  // Every derived class of ProcessingStep must implement a static create() function.
+  // Defining it here is not possible as virtual static function are not possible in C++.
+  // An alternative would be to use the Curiously Recurring Template Pattern (CRTP) but this approach would 
+  // require an additional abstract parent class because with CRTP the ProcessingStep class would become
+  // a template class which prevents the storage of all derived classes inside a single container.
+
+  // virtual static std::shared_ptr<ProcessingStep> create() = 0; // TODO: rename getInstance()?
 
   virtual void process(ALuint openAlId, 
     std::shared_ptr<std::map<std::string, std::any>> settings,
     std::shared_ptr<std::vector<std::string>> failedSettings) = 0;
- 
+
   virtual bool requiresUpdate() const = 0;
 
   virtual void update() = 0;
