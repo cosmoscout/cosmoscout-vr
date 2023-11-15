2dd9dc0f6a96464c50d8c4147b1d23539d91d1aa
florianSasz <100100037+florianSasz@users.noreply.github.com>
2023-11-16 00:06:53 +0100

    :wrench: Use common function for .wav header

--- src/cs-audio/internal/FileReader.cpp
+++ src/cs-audio/internal/FileReader.cpp
@@ -21,59 +21,13 @@ namespace cs::audio {
 
 bool FileReader::loadWAV(std::string fileName, WavContainer& wavContainer)
 {
-  char fileBuffer[4];
-  std::ifstream in(fileName, std::ios::binary);
-  
-  // check if it is a valid wave file:
-  in.read(fileBuffer, 4);
-  if (strncmp(fileBuffer, "RIFF", 4) != 0) {
+  if (!readWAVHeader(fileName, wavContainer)) {
     return false;
   }
-  
-  in.read(fileBuffer, 4); // ChunkSize            -- RIFF chunk descriptor
-  in.read(fileBuffer, 4); // Format
-  in.read(fileBuffer, 4); // SubChunk 1 id        -- fmt sub-chunk
-  in.read(fileBuffer, 4); // SubChunk 1 size
-  in.read(fileBuffer, 2); // AudioFormat
-  in.read(fileBuffer, 2); // Number Channels
-  wavContainer.numberChannels = convertToInt(fileBuffer, 2);
-  in.read(fileBuffer, 4); // Sample Rate
-  wavContainer.sampleRate = convertToInt(fileBuffer, 4);
-  in.read(fileBuffer, 4); // Byte Rate
-  in.read(fileBuffer, 2); // Block Align
-  in.read(fileBuffer, 2); // Bits per Sample
-  wavContainer.bitsPerSample = convertToInt(fileBuffer, 2);
-  in.read(fileBuffer, 4); // SubChunk 2 id        -- data sub-chunk
-  in.read(fileBuffer, 4); // SubChunk 2 Size
-  wavContainer.size = convertToInt(fileBuffer, 4);  
-  // wavContainer.pcm = std::vector<char>(wavContainer.size);
-  // in.read(wavContainer.pcm.data(), wavContainer.size); // data
+  std::ifstream in(fileName, std::ios::binary);
+  // move reader to the data chunk
+  in.seekg(44);
 
-  // Mono
-  if (wavContainer.numberChannels == 1) {
-    switch (wavContainer.bitsPerSample) {
-      case 8:
-        wavContainer.format = AL_FORMAT_MONO8;
-        break;
-      case 16:
-        wavContainer.format = AL_FORMAT_MONO16;
-        break;
-      case 32:
-        wavContainer.format = AL_FORMAT_MONO_FLOAT32;
-    }
-  // Stereo
-  } else {
-    switch (wavContainer.bitsPerSample) {
-      case 8:
-        wavContainer.format = AL_FORMAT_STEREO8;
-        break;
-      case 16:
-        wavContainer.format = AL_FORMAT_STEREO16;
-        break;
-      case 32:
-        wavContainer.format = AL_FORMAT_STEREO_FLOAT32;
-    }
-  }
   if (wavContainer.bitsPerSample == 32) {
     auto charData = std::vector<char>(wavContainer.size);
     in.read(charData.data(), wavContainer.size); // data
@@ -163,7 +117,7 @@ bool FileReader::readWAVHeader(std::string fileName, WavContainer& wavContainer)
         wavContainer.format = AL_FORMAT_STEREO16;
     }
   }
-  return false;
+  return true;
 }
 
 int FileReader::convertToInt(char* buffer, int len)
