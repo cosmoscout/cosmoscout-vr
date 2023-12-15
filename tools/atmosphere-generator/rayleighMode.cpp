6716379cf5c90fece4cf00706c94abdc1409d4af
Simon Schneegans <simon.schneegans@dlr.de>
2023-12-15 09:59:02 +0100

    :tada: Add Penndorf extinction table to Rayleigh mode

--- tools/atmosphere-generator/rayleighMode.cpp
+++ tools/atmosphere-generator/rayleighMode.cpp
@@ -17,19 +17,32 @@
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// Values from Table III in Penndorf 1957 "Tables of the Refractive Index for
+// Standard Air and the Rayleigh Scattering Coefficient for the Spectral
+// Region between 0.2 and 20.0 μ and Their Application to Atmospheric Optics".
+static const float PENNDORF[48] = {70.45E-6, 62.82E-6, 56.20E-6, 50.43E-6, 45.40E-6, 40.98E-6,
+    37.08E-6, 33.65E-6, 30.60E-6, 27.89E-6, 25.48E-6, 23.33E-6, 21.40E-6, 19.66E-6, 18.10E-6,
+    16.69E-6, 15.42E-6, 14.26E-6, 13.21E-6, 12.26E-6, 11.39E-6, 10.60E-6, 9.876E-6, 9.212E-6,
+    8.604E-6, 8.045E-6, 7.531E-6, 7.057E-6, 6.620E-6, 6.217E-6, 5.844E-6, 5.498E-6, 5.178E-6,
+    4.881E-6, 4.605E-6, 4.348E-6, 4.109E-6, 3.886E-6, 3.678E-6, 3.484E-6, 3.302E-6, 3.132E-6,
+    2.973E-6, 2.824E-6, 2.684E-6, 2.583E-6, 2.481E-6, 2.380E-6};
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 int rayleighMode(std::vector<std::string> const& arguments) {
 
-  bool        cPrintHelp      = false;
-  std::string cOutput         = "rayleigh";
-  bool        cPenndorf       = false;
-  double      cIoR            = 1.00028276;
-  double      cNumberDensity  = 2.68731e25;
-  double      cDepolarization = 0.0;
-  std::string cLambdas        = "";
-  double      cMinLambda      = 0.36e-6;
-  double      cMaxLambda      = 0.83e-6;
-  int32_t     cLambdaSamples  = 15;
-  int32_t     cThetaSamples   = 91;
+  bool        cPrintHelp          = false;
+  std::string cOutput             = "rayleigh";
+  bool        cPenndorfPhase      = false;
+  bool        cPenndorfExtinction = false;
+  double      cIoR                = 1.00028276;
+  double      cNumberDensity      = 2.68731e25;
+  double      cDepolarization     = 0.0;
+  std::string cLambdas            = "";
+  double      cMinLambda          = 0.36e-6;
+  double      cMaxLambda          = 0.83e-6;
+  int32_t     cLambdaSamples      = 15;
+  int32_t     cThetaSamples       = 91;
 
   // First configure all possible command line options.
   cs::utils::CommandLine args("Here are the available options:");
@@ -37,7 +50,10 @@ int rayleighMode(std::vector<std::string> const& arguments) {
       "The scattering data will be written to <name>_phase.csv, <name>_scattering.csv, and "
       "<name>_absorption.csv, respectively (default: \"" +
           cOutput + "\").");
-  args.addArgument({"--penndorf"}, &cPenndorf, "Use the Penndorf phase function (default: false)");
+  args.addArgument(
+      {"--penndorf-phase"}, &cPenndorfPhase, "Use the Penndorf phase function (default: false)");
+  args.addArgument({"--penndorf-extinction"}, &cPenndorfExtinction,
+      "Use the Penndorf extinction tables (default: false)");
   args.addArgument(
       {"--ior"}, &cIoR, fmt::format("The index of refraction of the gas (default: {}).", cIoR));
   args.addArgument({"-n", "--number-density"}, &cNumberDensity,
@@ -94,12 +110,33 @@ int rayleighMode(std::vector<std::string> const& arguments) {
   for (double lambda : lambdas) {
 
     // Print scattering coefficient.
-    double f = std::pow((cIoR * cIoR - 1.0), 2.0) * (6.0 + 3.0 * cDepolarization) /
-               (6.0 - 7.0 * cDepolarization);
-    double beta_sca =
-        8.0 / 3.0 * std::pow(glm::pi<double>(), 3.0) * f / (cNumberDensity * std::pow(lambda, 4));
+    if (cPenndorfExtinction) {
 
-    scatteringOutput << fmt::format("{},{}", lambda, beta_sca) << std::endl;
+      std::vector<double> penndorf;
+      for (int i = 0; i < 48; ++i) {
+        // The above values are for T_0=0°C. For T=15°C, a correction factor
+        // T_0 / T must be applied (Eq. (12) in Penndorf paper).
+        constexpr double T_0 = 273.16;
+        constexpr double T   = T_0 + 15.0;
+        penndorf.push_back(PENNDORF[i] * (T_0 / T));
+      }
+
+      const double minLambda = 0.36e-6;
+      const double maxLambda = 0.83e-6;
+
+      double beta_sca = common::interpolate(penndorf, minLambda, maxLambda, lambda);
+
+      scatteringOutput << fmt::format("{},{}", lambda, beta_sca) << std::endl;
+
+    } else {
+
+      double f = std::pow((cIoR * cIoR - 1.0), 2.0) * (6.0 + 3.0 * cDepolarization) /
+                 (6.0 - 7.0 * cDepolarization);
+      double beta_sca =
+          8.0 / 3.0 * std::pow(glm::pi<double>(), 3.0) * f / (cNumberDensity * std::pow(lambda, 4));
+
+      scatteringOutput << fmt::format("{},{}", lambda, beta_sca) << std::endl;
+    }
 
     // Absorption is always zero.
     absorptionOutput << fmt::format("{},{}", lambda, 0.0) << std::endl;
@@ -108,7 +145,7 @@ int rayleighMode(std::vector<std::string> const& arguments) {
     for (int32_t i(0); i < totalAngles; ++i) {
       double theta = i * glm::pi<double>() / (totalAngles - 1);
       double phase =
-          cPenndorf
+          cPenndorfPhase
               ? 0.7629 / (4.0 * glm::pi<double>()) * (1.0 + 0.932 * std::pow(std::cos(theta), 2.0))
               : 3.0 / (16.0 * glm::pi<double>()) * (1.0 + std::pow(std::cos(theta), 2.0));
       phaseOutput << fmt::format(",{}", phase);
