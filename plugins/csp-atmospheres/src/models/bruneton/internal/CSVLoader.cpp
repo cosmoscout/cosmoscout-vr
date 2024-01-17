47091a27543327ce78db209d25b54e0a787a5c0d
Simon Schneegans <simon.schneegans@dlr.de>
2024-01-17 10:16:26 +0100

    :memo: Add comments to CSV loader

--- plugins/csp-atmospheres/src/models/bruneton/internal/CSVLoader.cpp
+++ plugins/csp-atmospheres/src/models/bruneton/internal/CSVLoader.cpp
@@ -7,17 +7,20 @@
 
 #include "CSVLoader.hpp"
 
-#include <regex>
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <regex>
 
 namespace csp::atmospheres::models::bruneton::internal {
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 namespace {
 
-/**
- * https://www.techiedelight.com/trim-string-cpp-remove-leading-trailing-spaces/
- */
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Helper methods to trim left and right whitespace from a string. Based on:
+// https://www.techiedelight.com/trim-string-cpp-remove-leading-trailing-spaces/
 std::string ltrim(const std::string& s) {
   return std::regex_replace(s, std::regex("^\\s+"), std::string(""));
 }
@@ -30,6 +33,25 @@ std::string trim(const std::string& s) {
   return ltrim(rtrim(s));
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Helper method to remove consecutive whitespaces. Based on:
+// http://www.cplusplus.com/forum/beginner/185953/
+std::string removeMultiWhitespaces(const std::string& s) {
+  return std::regex_replace(s, std::regex("\\s{2,}"), std::string(" "));
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Helper method to replace tabs by spaces.
+std::string replaceTabsWithWhitespaces(std::string& s) {
+  std::replace(std::begin(s), std::end(s), '\t', ' ');
+  return s;
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Safe conversion of strings to floats.
 float stringToFloat(std::string const& s) {
   try {
     return std::stof(s);
@@ -40,24 +62,53 @@ float stringToFloat(std::string const& s) {
   return 0.f;
 }
 
-/**
- * http://www.cplusplus.com/forum/beginner/185953/
- */
-std::string removeMultiWhitespaces(const std::string& s) {
-  return std::regex_replace(s, std::regex("\\s{2,}"), std::string(" "));
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Helper method to read a line of text from an input stream and split it at the given delimiters.
+std::vector<std::string> lineToArray(std::stringstream& ss, char delimiter) {
+  auto result = std::vector<std::string>();
+
+  std::string text;
+  while (std::getline(ss, text, delimiter)) {
+    result.push_back(text);
+  }
+
+  return result;
 }
 
-std::string replaceTabsWithWhitespaces(std::string& s) {
-  std::replace(std::begin(s), std::end(s), '\t', ' ');
-  return s;
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Reads the given file line by line and calls the given callback for each line.
+void readLines(std::string const&                          filename,
+    std::function<void(long lineNumber, std::string line)> lineConsumer) {
+  std::ifstream file(filename);
+
+  if (!file.is_open()) {
+    throw std::runtime_error("Could not open file " + filename);
+  }
+
+  if (!file.good()) {
+    return;
+  }
+
+  std::string line;
+  long        lineNumber = 0;
+  while (std::getline(file, line)) {
+    lineConsumer(lineNumber++, line);
+  }
+  file.close();
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 } // namespace
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 std::vector<double> CSVLoader::readDensity(std::string const& filename, uint32_t& densityCount) {
   std::vector<double> result;
 
-  bool checkDensities = densityCount != 0;
-
+  // Read the file line-by-line.
   readLines(filename, [&](long lineNumber, std::string line) {
     std::stringstream ss(trim(removeMultiWhitespaces(replaceTabsWithWhitespaces(line))));
 
@@ -70,26 +121,31 @@ std::vector<double> CSVLoader::readDensity(std::string const& filename, uint32_t
     result.push_back(stringToFloat(elements[0]));
   });
 
-  if (checkDensities) {
+  // We compare the number of read densities to the target number if given.
+  if (densityCount != 0) {
     if (densityCount != result.size()) {
       throw std::runtime_error(
           "Failed to read density from '" + filename +
           "': Number of density values differs from a previously loaded data set!");
     }
+  } else {
+    densityCount = result.size();
   }
-
-  densityCount = result.size();
 
   return result;
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 std::vector<std::vector<double>> CSVLoader::readPhase(
     std::string const& filename, std::vector<double>& wavelengths) {
 
   std::vector<std::vector<double>> phaseFunctions;
 
+  // We will check the read wavelengths if some target wavelengths are given.
   bool checkWavelengths = !wavelengths.empty();
 
+  // Read the file line-by-line.
   readLines(filename, [&](long lineNumber, std::string line) {
     std::stringstream ss(trim(removeMultiWhitespaces(replaceTabsWithWhitespaces(line))));
 
@@ -144,12 +200,16 @@ std::vector<std::vector<double>> CSVLoader::readPhase(
   return spectra;
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 std::vector<double> CSVLoader::readExtinction(
     std::string const& filename, std::vector<double>& wavelengths) {
   std::vector<double> result;
 
+  // We will check the read wavelengths if some target wavelengths are given.
   bool checkWavelengths = !wavelengths.empty();
 
+  // Read the file line-by-line.
   readLines(filename, [&](long lineNumber, std::string line) {
     std::stringstream ss(trim(removeMultiWhitespaces(replaceTabsWithWhitespaces(line))));
 
@@ -179,31 +239,6 @@ std::vector<double> CSVLoader::readExtinction(
   return result;
 }
 
-std::vector<std::string> CSVLoader::lineToArray(std::stringstream& ss, char delimiter) {
-  auto result = std::vector<std::string>();
-
-  std::string text;
-  while (std::getline(ss, text, delimiter)) {
-    result.push_back(text);
-  }
-  return result;
-}
-
-void CSVLoader::readLines(std::string const&               filename,
-    std::function<void(long lineNumber, std::string line)> lineConsumer) {
-  std::ifstream file(filename);
-
-  if (!file.is_open())
-    throw std::runtime_error("Could not open file " + filename);
-  if (!file.good())
-    return;
-
-  std::string line;
-  long        lineNumber = 0;
-  while (std::getline(file, line)) {
-    lineConsumer(lineNumber++, line);
-  }
-  file.close();
-}
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 } // namespace csp::atmospheres::models::bruneton::internal
--- plugins/csp-atmospheres/src/models/bruneton/internal/CSVLoader.hpp
+++ plugins/csp-atmospheres/src/models/bruneton/internal/CSVLoader.hpp
@@ -14,18 +14,59 @@
 
 namespace csp::atmospheres::models::bruneton::internal {
 
+/// This static class is used to load the phase functions, scattering & absorption coefficients, as
+/// well as the density distributions of atmospheric particles from CSV files.
 class CSVLoader {
  public:
+  /// This loads a density distribution sampled at evenly spaced altitudes from the given CSV file.
+  /// The first returned value corresponds to the bottom-most density in the atmosphere. If the
+  /// densityCount parameter is initialized to 0, it will be filled with the number of densities
+  /// read from the file. If it is initialized to a value != 0, the method will compare the number
+  /// of read densities to this number and throw an exception if this doesn't match. This is used to
+  /// ensure that all constituents of an atmosphere are sampled at the same altitudes. The content
+  /// of the CSV file could look like this:
+  ///
+  /// density
+  /// 1.0
+  /// 0.9369102299935942
+  /// 0.8778007790666494
+  /// ...
+  /// 0.0
+  ///
   static std::vector<double> readDensity(std::string const& filename, uint32_t& densityCount);
+
+  /// This loads a 2D wavelength-dependent phase function sampled a specific angles and wavelengths
+  /// from a CSV file. The method basically returns the columns from the CSV file. This means, for
+  /// each phase function angle, the outer vector contains a spectrum of intensity values. If the
+  /// wavelengths parameter is initially empty, it will be filled with the wavelengths read from the
+  /// file. If it already contains some values, the method will compare them to the newly read
+  /// wavelengths and throw an exception if they don't match. This is used to ensure that all
+  /// constituents of an atmosphere are sampled at the same wavelengths. The units for the
+  /// wavelengths should be [m]. The content of the CSV file could look like this:
+  ///
+  /// lambda,  0.0,   1.0,   2.0,   ..., 180.0
+  /// 4.4e-07, 0.933, 0.931, 0.923, ..., 0.022
+  /// 5.5e-07, 0.257, 0.257, 0.256, ..., 0.025
+  /// 6.8e-07, 0.091, 0.091, 0.091, ..., 0.045
+  ///
   static std::vector<std::vector<double>> readPhase(
       std::string const& filename, std::vector<double>& wavelengths);
+
+  /// This loads wavelength-dependent scattering coefficients (beta_sca) or absorption coefficients
+  /// (beta_abs) from CSV files. If the wavelengths parameter is initially empty, it will be filled
+  /// with the wavelengths read from the file. If it already contains some values, the method will
+  /// compare them to the newly read wavelengths and throw an exception if they don't match. This is
+  /// used to ensure that all constituents of an atmosphere are sampled at the same wavelengths. The
+  /// units for the wavelengths should be [m], for the extinction coefficients [m^-1]. The content
+  /// of the CSV file could look like this:
+  ///
+  /// lambda,  beta_sca
+  /// 4.4e-07, 5.70e-07
+  /// 5.5e-07, 2.33e-07
+  /// 6.8e-07, 1.00e-07
+  ///
   static std::vector<double> readExtinction(
       std::string const& filename, std::vector<double>& wavelengths);
-
- private:
-  static std::vector<std::string> lineToArray(std::stringstream& ss, char delimiter);
-  static void                     readLines(std::string const&                   filename,
-                          std::function<void(long lineNumber, std::string line)> lineConsumer);
 };
 
 } // namespace csp::atmospheres::models::bruneton::internal
