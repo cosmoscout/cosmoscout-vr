f334d8f178e7140cdae3671ddb99cfd0cc5fbe92
Florian Sass <100100037+florianSasz@users.noreply.github.com>
2024-01-31 13:40:37 +0100

    :wrench: Add TransferFunction ToDos

--- plugins/csp-visual-query/src/operation-nodes/TransferFunction/TransferFunction.cpp
+++ plugins/csp-visual-query/src/operation-nodes/TransferFunction/TransferFunction.cpp
@@ -7,6 +7,11 @@
 
 #include "../../../../src/cs-utils/filesystem.hpp"
 #include "TransferFunction.hpp"
+#include <cmath>
+#include <algorithm>
+
+// testing:
+#include <fstream>
 
 namespace csp::visualquery {
 
@@ -36,7 +41,67 @@ std::string const& TransferFunction::getName() const {
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 void TransferFunction::process() {
+  auto minMax = readInput<std::pair<double, double>>("minMax", std::pair<double,double>(0, 256));
+  /*
+
+  TODO:
+  - statistics node (min, max, average)
+  - WCS min/max aus anfrage?
+  - min/max input in renderer
+  - min/max in transfer ohne fkt
+
+
+  if (minMax.first != 0 || minMax.second != 256) {
+    // scale the lut to the difference of the min and max value
+    const int diff = static_cast<const int>(std::abs(minMax.second - minMax.first));
+    std::vector<glm::vec4> scaledLUT(diff);
+
+    for (int i = 0; i < diff; ++i) {
+
+      // get current position in scaledLUT
+      double scaledPos = static_cast<double>(i) / diff;
+      
+      // get closest indicies to scaledLUT pos in mLUT
+      double mLutPos = scaledPos * 255;
+      int lowerIndex = static_cast<int>(std::floor(mLutPos));
+      int upperIndex = static_cast<int>(std::ceil(mLutPos));
+
+      // ratio between lowerIndex and upperIndex
+      double t = mLutPos - lowerIndex;
+
+      // lerp both indicies
+      scaledLUT[i] = lerpVec4(mLut[lowerIndex], mLut[upperIndex], t);
+    }
+
+    {
+      nlohmann::json ogLUTJson;
+      ogLUTJson["lut"] = mLut;
+      std::ofstream file("C:/Users/sass_fl/ogLUT.json");
+      file << ogLUTJson;
+
+      nlohmann::json scaledLUTJson;
+      scaledLUTJson["lut"] = scaledLUT;
+      std::ofstream file2("C:/Users/sass_fl/scaledLUT.json");
+      file2 << scaledLUTJson;
+    }
+
+    mLut = scaledLUT;
+  }*/
+
+  std::cout << "lut size: " << mLut.size() << std::endl;
   writeOutput("lut", mLut);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+glm::vec4 TransferFunction::lerpVec4(glm::vec4 v0, glm::vec4 v1, double t) {
+  // TODO: lerp colors via HSV and not RGB
+  return glm::vec4(
+    v0[0] + t * (v1[0] - v0[0]),
+    v0[1] + t * (v1[1] - v0[1]),
+    v0[2] + t * (v1[2] - v0[2]),
+    v0[3] + t * (v1[3] - v0[3])
+  );
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -47,7 +112,6 @@ void TransferFunction::onMessageFromJS(nlohmann::json const& message) {
   }
 
   cs::core::Settings::deserialize(message, "lut", mLut);
-
   process();
 }
 
