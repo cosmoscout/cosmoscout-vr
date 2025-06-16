da0f057d67a687a50fedab93e777eb7cc9525b3d
Kuzminykh <alexander.kuzminykh@dlr.de>
2025-06-16 17:23:16 +0200

    :wrench: Increase default EV range

--- src/cs-core/Settings.hpp
+++ src/cs-core/Settings.hpp
@@ -459,7 +459,7 @@ class CS_CORE_EXPORT Settings {
 
     /// The range from which to choose values for the auto exposure. Measured in exposure values
     /// (EV).
-    utils::DefaultProperty<glm::vec2> pAutoExposureRange{glm::vec2(-13.F, 9.F)};
+    utils::DefaultProperty<glm::vec2> pAutoExposureRange{glm::vec2(-16.F, 12.F)};
 
     /// An additional exposure control which is applied after auto exposure. Has no effect if HDR
     /// rendering is disabled. Measured in exposure values (EV).
