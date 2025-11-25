187e7582baac4584946b405ab23c762318fdcc4d
Jonathan Fritsch <jonathan.fritsch@dlr.de>
2025-11-25 17:00:06 +0100

    :wrench: Use canvas for display instead of img

--- plugins/csp-satellites/gui/js/csp-satellites.js
+++ plugins/csp-satellites/gui/js/csp-satellites.js
@@ -37,24 +37,30 @@
     }
 
     _fetchImage() {
-        const width = 200;
-        const height = 200;
-        const delay = 10;
-        //this._viewImg.src = `${this._renderServer}/capture?width=${width}&height=${height}&delay=${delay}&gui=false#${new Date().getTime()}`;
+        const ctx = this._viewCtx;
+        const thiz = this;
+
+        this._resetDate();
 
         const params = new URLSearchParams();
-        params.append("width", "600");
-        params.append("height", "600");
+        params.append("width", "200");
+        params.append("height", "200");
         params.append("gui", "false");
-        params.append("delay", "10");
-        params.append("format", "jpeg");
+        params.append("delay", "0");
+        params.append("format", "png");
         fetch(`${this._renderServer}/capture?${params}`)
             .then(res => res.blob())
             .then(blob => {
-                console.log(blob.size);
-                console.log(blob.type);
-                const imgData = URL.createObjectURL(blob);
-                //this._viewImg.src = imgData;
+                const reader = new FileReader();
+                reader.onload = function() {
+                    const img = new Image();
+                    img.src = reader.result;
+                    img.onload = function() {
+                        ctx.drawImage(img, 0, 0, 200, 200);
+                        thiz._fetchImage();
+                    };
+                };
+                reader.readAsDataURL(blob);
             })
             .catch(e => console.error(`Error fetching satellite view: ${e}`));
     }
@@ -63,14 +69,11 @@
         this._connectionEstablished = false;
         this._viewDiv = CosmoScout.gui.loadTemplateContent('satellite-view-template');
         document.getElementById('cosmoscout').appendChild(this._viewDiv);
-        this._viewImg = document.querySelector('#satellite-view-img');
+        this._viewCanvas = document.getElementById('satellite-view-canvas');
+        this._viewCtx = this._viewCanvas.getContext("2d");
         this._renderServer = "http://localhost:9002";
 
         this._resetObserver();
-
-        this._viewImg.addEventListener("load", () => {
-            this._fetchImage();
-        });
     }
 
     deinit() {
