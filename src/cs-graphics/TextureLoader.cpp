cef90a099d45d0525c27b68d6cdc8996a03b4939
Jonas Gilg <jonas.gilg@dlr.de>
2026-07-28 15:27:18 +0200

    :wrench: Add more libs to vcpkg config.

--- src/cs-graphics/TextureLoader.cpp
+++ src/cs-graphics/TextureLoader.cpp
@@ -13,7 +13,6 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
 #include <stb_image.h>
-#include <stb_image_resize.h>
 #include <stb_image_write.h>
 #undef STB_IMAGE_IMPLEMENTATION
 #undef STB_IMAGE_WRITE_IMPLEMENTATION
