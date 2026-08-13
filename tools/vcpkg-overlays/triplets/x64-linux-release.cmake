f3302a3bfc6a2fdea50c14e303adaeb36e474ab1
Jonas Gilg <jonas.gilg@dlr.de>
2026-08-13 14:17:55 +0200

    :green_heart: Fix REUSE errors [run-ci].

--- tools/vcpkg-overlays/triplets/x64-linux-release.cmake
+++ tools/vcpkg-overlays/triplets/x64-linux-release.cmake
@@ -1,3 +1,10 @@
+# ------------------------------------------------------------------------------------------------ #
+#                                This file is part of CosmoScout VR                                #
+# ------------------------------------------------------------------------------------------------ #
+
+# SPDX-FileCopyrightText: German Aerospace Center (DLR) <cosmoscout@dlr.de>
+# SPDX-License-Identifier: CC0-1.0
+
 set(VCPKG_CMAKE_SYSTEM_NAME   Linux)
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_LIBRARY_LINKAGE     dynamic)
