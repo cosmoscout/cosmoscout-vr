1862c052cf0d0d391d0c01a75b472df2bf4be150
Jonas Gilg <jonas.gilg@dlr.de>
2026-08-13 16:20:24 +0200

    :sparkles: Run clang-format.

--- resources/gui/js/apis/statusbar.js
+++ resources/gui/js/apis/statusbar.js
@@ -253,8 +253,7 @@ class StatusbarApi extends IApi {
               // completion.
               this._suggestionField.insertAdjacentHTML("beforeend", `<span class='${classNames}'
                        onclick='CosmoScout.statusbar._setCompletion(${prefixBegin}, ${prefixEnd}, 
-                                                              ${finalCursorPos}, "${
-                                                                        completion}");'>
+                                                              ${finalCursorPos}, "${completion}");'>
                        ${element}
                 </span>`);
             });
